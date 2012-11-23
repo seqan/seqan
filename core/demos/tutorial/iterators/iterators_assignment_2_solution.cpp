9db2a799386d04a8e2f3ba7c73bf3f7f1278e2fb
sabrina7 <sabrina7@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-11-23 10:49:50 +0000

    [DOC] Moved demo files for iterators from extras to core.
    git-svn-id: http://svn.seqan.de/seqan/trunk@13258 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ core/demos/tutorial/iterators/iterators_assignment_2_solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <seqan/sequence.h>
+#include <seqan/file.h>
+
+using namespace seqan;
+
+int main()
+{
+    Dna5String genome = "ANTGGTTNCAACNGTAANTGCTGANNNACATGTNCGCGTGTA";
+    
+    Iterator<Dna5String, Rooted >::Type it = begin(genome);
+
+    for (; !atEnd(it); goNext(it)){
+        if (getValue(it) == 'N')
+            value(it) = 'A';
+    }
+    std::cout << "Modified genome: " << genome << std::endl;
+    return 0;
+}
