5fe952c6a48e72c9d51444bf38530c68d2a60884
jsinger <jsinger@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-04-05 09:18:14 +0000

    [INTERNAL] Added a script to adjust the copyright year of all h and cpp files.
    git-svn-id: http://svn.seqan.de/seqan/trunk@13854 e6417c60-b987-48fd-844e-b20f0fcc1017

--- misc/adjust_copy_right_year.sh
+++ misc/adjust_copy_right_year.sh
@@ -3,7 +3,7 @@
 #This file replaces in every .h and .cpp file in include and tests directories of core and trunk an old copyright line with a new one. 
 #Note: You have to replace the until date in this document
 #Note: Call this file from the trunk directory
-for i in $( find core/{include,tests} extras/{include,tests} -name "*.h" -o -name "*.cpp" ); do
+for i in $( find core/{include,tests, demos} extras/{include,tests, demos} -name "*.h" -o -name "*.cpp" ); do
     sed -i '' 's/Copyright (c) 20..-20.., Knut Reinert, FU Berlin/Copyright (c) 2006-2013, Knut Reinert, FU Berlin/g' $i
 done
 
