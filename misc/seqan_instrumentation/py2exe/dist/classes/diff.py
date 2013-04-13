0bb9f559bc9d93d9e1a36c793bf40e698f92d64a
bjornson <bjornson@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-04-13 09:27:38 +0000

    [INTERNAL] Updated py2exe
    git-svn-id: http://svn.seqan.de/seqan/trunk@13902 e6417c60-b987-48fd-844e-b20f0fcc1017

--- misc/seqan_instrumentation/py2exe/dist/classes/diff.py
+++ misc/seqan_instrumentation/py2exe/dist/classes/diff.py
@@ -3,90 +3,98 @@ import subprocess
 import glob
 from datetime import datetime
 import socket
-import requests
 import re # regular expressions
 import zipfile
 import hashlib
 
 class Diff(object):
-	def __init__(self, bin_dir, results_dir, root_dir, from_dir_local, to_dir_local, id, excluded_resources):
-		self.results_dir = results_dir
-		self.root_dir = root_dir
-		self.from_dir_local = from_dir_local
-		self.to_dir_local = to_dir_local
-		self.id = id
-		self.excluded_resources = excluded_resources
-		self.bin_dir = bin_dir
-		
-	def get_timezone_string(self):
-		import time
-		offset = -time.timezone
-		if(time.localtime().tm_isdst):
-			offset = -time.altzone
-		hours = abs(offset)/3600
-		minutes = (abs(offset)%3600)/60
-		return ("+" if offset >= 0 else "-") + str(hours).rjust(2, "0") + str(minutes).rjust(2, "0")
+    def __init__(self, bin_dir, results_dir, root_dir, from_dir_local, to_dir_local, id, excluded_resources):
+        self.results_dir = results_dir
+        self.root_dir = root_dir
+        self.from_dir_local = from_dir_local
+        self.to_dir_local = to_dir_local
+        self.id = id
+        self.excluded_resources = excluded_resources
+        self.bin_dir = bin_dir
+        
+    def get_timezone_string(self):
+        import time
+        offset = -time.timezone
+        if(time.localtime().tm_isdst):
+            offset = -time.altzone
+        hours = abs(offset)/3600
+        minutes = (abs(offset)%3600)/60
+        return ("+" if offset >= 0 else "-") + str(hours).rjust(2, "0") + str(minutes).rjust(2, "0")
 
-	def get_diff_filename(self, datetime):
-		root_dir_hash = hashlib.md5(self.root_dir).hexdigest()[:4]
-		return self.results_dir + "/" + self.id.get() + "_" + root_dir_hash + "_" + datetime.strftime("%Y-%m-%dT%H-%M-%S.%f") + self.get_timezone_string() + ".diff.zip"
-	
-	def save_diff(self, filename):
-		# old parameters (to create a huge textual diff file: "-a", "-u", "-r", "-N"
-		# change the language to C to avoid unparseable non-english output 
-		# of localized shell tools
-		my_env = os.environ.copy()
-		my_env["LANG"] = "C"
-		if(os.name == "nt"):
-			p = subprocess.Popen([self.bin_dir + "/diffutils/bin/diff.exe", "-q", "-r", "-N"] + self.excluded_resources.get_exclude_list_diff() + ["." + self.from_dir_local, self.to_dir_local], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.root_dir, env=my_env)
-		else:
-			p = subprocess.Popen(["diff", "-q", "-r", "-N"] + self.excluded_resources.get_exclude_list_diff() + ["." + self.from_dir_local, self.to_dir_local], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.root_dir, env=my_env)
-		output = p.communicate()[0]
-		
-		zipFile = zipfile.ZipFile(filename, "w")
-		
-		for line in output.split("\n"):
-			match = re.search(" and \\.(.*) differ", line)
-			if(match and match.groups > 0):
-				currentFile = self.root_dir + match.group(1);
-				oldFile = self.root_dir + self.from_dir_local + match.group(1);
-				if(os.path.isfile(currentFile)):
-					zipFile.write(currentFile, match.group(1), zipfile.ZIP_STORED)
+    def get_diff_filename(self, datetime):
+        root_dir_hash = hashlib.md5(self.root_dir).hexdigest()[:4]
+        return self.results_dir + "/" + self.id.get() + "_" + root_dir_hash + "_" + datetime.strftime("%Y-%m-%dT%H-%M-%S.%f") + self.get_timezone_string() + ".diff.zip"
+    
+    def save_diff(self, filename):
+        # old parameters (to create a huge textual diff file: "-a", "-u", "-r", "-N"
+        # change the language to C to avoid unparseable non-english output 
+        # of localized shell tools
+        my_env = os.environ.copy()
+        my_env["LANG"] = "C"
+        if(os.name == "nt"):
+            p = subprocess.Popen([self.bin_dir + "/diffutils/bin/diff.exe", "-q", "-r", "-N"] + self.excluded_resources.get_exclude_list_diff() + ["." + self.from_dir_local, self.to_dir_local], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.root_dir, env=my_env)
+        else:
+            p = subprocess.Popen(["diff", "-q", "-r", "-N"] + self.excluded_resources.get_exclude_list_diff() + ["." + self.from_dir_local, self.to_dir_local], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.root_dir, env=my_env)
+        output = p.communicate()[0]
+        
+        zipFile = zipfile.ZipFile(filename, "w")
+        
+        for line in output.split("\n"):
+            match = re.search(" and \\.(.*) differ", line)
+            if(match and match.groups > 0):
+                currentFile = self.root_dir + match.group(1);
+                oldFile = self.root_dir + self.from_dir_local + match.group(1);
+                if(os.path.isfile(currentFile)):
+                    zipFile.write(currentFile, match.group(1), zipfile.ZIP_STORED)
 
-				#else:
-					# currentFile does not exist anymore
-					# ignore, since the old revision was already saved
-					
-		zipFile.close()
+                #else:
+                    # currentFile does not exist anymore
+                    # ignore, since the old revision was already saved
+                    
+        zipFile.close()
 
-	def sync(self, url):
-		# get remote file list
-		remote_files = []
-		
-		readFileListUrl = url + "/" + self.id.get()
-		try:
-			r = requests.get(readFileListUrl)
-			if(r.status_code != 200):
-				print("Connection to " + readFileListUrl + " failed with status code " + r.status_code + "! Retrying next time.")
-				return
-			remote_files = {}
-			for line in r.text.split("\n"):
-				file = line.strip().split("\t")
-				if(len(file) < 2): continue
-				remote_files[file[0]] = file[1]
-		except Exception as e:
-			print("Connection to " + readFileListUrl + " failed for an unknow reason! Please inform bjoern.kahlert@fu-berlin.de.")
-			print(e)
-			return
+    def sync(self, url):
+        import requests
+        
+        # get remote file list
+        remote_files = []
+        
+        readFileListUrl = url + "/" + self.id.get()
+        try:
+            r = requests.get(readFileListUrl, timeout=5.0)
+            if(r.status_code != 200):
+                print("Connection to " + readFileListUrl + " failed with status code " + r.status_code + "! Retrying next time.")
+                return
+            remote_files = {}
+            for line in r.text.split("\n"):
+                file = line.strip().split("\t")
+                if(len(file) < 2): continue
+                remote_files[file[0]] = file[1]
+        except requests.exceptions.ConnectionError as e:
+            print("\n!!! Connection to " + readFileListUrl + " failed! Retrying next time.") 
+            return       
+        except requests.exceptions.Timeout as e:
+            print("\n!!! Connection to " + readFileListUrl + " timed out! Retrying next time.")
+            return
+        except Exception as e:
+            print(type(e))
+            print("\n !!!Connection to " + readFileListUrl + " failed for an unknow reason! Please inform bjoern.kahlert@fu-berlin.de.")
+            print(e)
+            return
 
-		# upload all files that are not present on the ftp server
-		for filename in glob.glob(self.results_dir + "/" + self.id.get() + "_*"):
-			copy = True
-			if(os.path.basename(filename) in remote_files.keys()):
-				remote_size = int(remote_files[os.path.basename(filename)])
-				local_size = os.path.getsize(filename)
-				if(remote_size == local_size): copy = False
-			
-			if(copy):
-				files = {self.id.get(): open(filename, 'rb')}
-				r = requests.post(url, files=files)
+        # upload all files that are not present on the ftp server
+        for filename in glob.glob(self.results_dir + "/" + self.id.get() + "_*"):
+            copy = True
+            if(os.path.basename(filename) in remote_files.keys()):
+                remote_size = int(remote_files[os.path.basename(filename)])
+                local_size = os.path.getsize(filename)
+                if(remote_size == local_size): copy = False
+            
+            if(copy):
+                files = {self.id.get(): open(filename, 'rb')}
+                r = requests.post(url, files=files)
--- misc/seqan_instrumentation/py2exe/dist/classes/stats.py
+++ misc/seqan_instrumentation/py2exe/dist/classes/stats.py
@@ -11,74 +11,74 @@ sys.path.append(os.path.abspath(os.path.split(inspect.getfile(inspect.currentfra
 import simplejson as json
 
 class Stats(object):
-	CMakeCachePathKey = "CMakeCachePath"
+    CMakeCachePathKey = "CMakeCachePath"
+
+    def __init__(self, cMake_binary_dir, src_dir, stats_cache):
+        self.cMake_binary_dir = cMake_binary_dir
+        self.src_dir = src_dir
+        self.stats_cache = stats_cache
+        self.stats = {}
+        try:
+            if os.path.isfile(self.stats_cache):
+                self.load()
+        except:
+            pass
+
+        self.get_os()
+        self.get_machine()
+        self.get_ip()
+
+    def get(self, key):
+        return self.stats[key]
+
+    def set(self, key, value):
+        if not key == None:
+            self.stats[key] = value
+
+    def load(self):
+        f = open(self.stats_cache, "r")
+        self.stats = json.load(f)
+        f.close()
+
+    def save(self, key=None, value=None):
+        self.set(key, value)
+
+        f = open(self.stats_cache, "w")
+        json.dump(self.stats, f, indent = 4)
+        f.close()
+
+        cMakeCache_path = self.get_cMakeCache_path()
+        if not cMakeCache_path is None and os.path.isfile(cMakeCache_path):
+            shutil.copyfile(cMakeCache_path, self.get_cMakeCache_copy_path())
+
+    def needs_refresh(self, stat):
+        return not stat in self.stats or self.stats[stat] is None or len(self.stats[stat]) == 0
+    
+    def get_os(self):
+        if(self.needs_refresh("os")):
+            self.stats["os"] = { "sys.platform": sys.platform, "platform": platform.platform(), "system": platform.system(), "release": platform.release(), "version": platform.version(), "node": platform.node() }
+            self.save()
+        return self.stats["os"]
+
+    def get_machine(self):
+        if(self.needs_refresh("machine")):
+            self.stats["machine"] = { "machine": platform.machine(), "processor": platform.processor(), "architecture": [ platform.architecture(), "64" if sys.maxsize > 2**32 else "no64" ]}
+            self.save()
+        return self.stats["machine"]
         
-	def __init__(self, cMake_binary_dir, src_dir, stats_cache):
-		self.cMake_binary_dir = cMake_binary_dir
-		self.src_dir = src_dir
-		self.stats_cache = stats_cache
-		self.stats = {}
-		try:
-			if os.path.isfile(self.stats_cache):
-				self.load()
-		except:
-			pass
+    def get_ip(self):
+        if(self.needs_refresh("ip")):
+            self.stats["ip"] = []
+            for addrinfo in socket.getaddrinfo(socket.gethostname(), None, 0, 0, socket.SOL_TCP):
+                self.stats["ip"].append(addrinfo[4][0])
+            self.save()
+        return self.stats["ip"]
 
-		self.get_os()
-		self.get_machine()
-		self.get_ip()
+    def get_cMakeCache_path(self):
+        cMakeCache_paths = map(os.path.normpath, glob.glob(self.cMake_binary_dir + "/../*/CMakeCache.txt"))
+        return None if len(cMakeCache_paths) == 0 else cMakeCache_paths[0]
 
-	def get(self, key):
-		return self.stats[key]
-
-	def set(self, key, value):
-		if not key == None:
-			self.stats[key] = value
-
-	def load(self):
-		f = open(self.stats_cache, "r")
-		self.stats = json.load(f)
-		f.close()
-
-	def save(self, key=None, value=None):
-                self.set(key, value)
-                
-		f = open(self.stats_cache, "w")
-		json.dump(self.stats, f, indent = 4)
-		f.close()
-
-		cMakeCache_path = self.get_cMakeCache_path()
-		if not cMakeCache_path is None and os.path.isfile(cMakeCache_path):
-			shutil.copyfile(cMakeCache_path, self.get_cMakeCache_copy_path())
-		
-	def needs_refresh(self, stat):
-		return not stat in self.stats or self.stats[stat] is None or len(self.stats[stat]) == 0
-	
-	def get_os(self):
-		if(self.needs_refresh("os")):
-			self.stats["os"] = { "sys.platform": sys.platform, "platform": platform.platform(), "system": platform.system(), "release": platform.release(), "version": platform.version(), "node": platform.node() }
-			self.save()
-		return self.stats["os"]
-
-	def get_machine(self):
-		if(self.needs_refresh("machine")):
-			self.stats["machine"] = { "machine": platform.machine(), "processor": platform.processor(), "architecture": [ platform.architecture(), "64" if sys.maxsize > 2**32 else "no64" ]}
-			self.save()
-		return self.stats["machine"]
-	    
-	def get_ip(self):
-		if(self.needs_refresh("ip")):
-			self.stats["ip"] = []
-			for addrinfo in socket.getaddrinfo(socket.gethostname(), None, 0, 0, socket.SOL_TCP):
-				self.stats["ip"].append(addrinfo[4][0])
-			self.save()
-		return self.stats["ip"]
-
-	def get_cMakeCache_path(self):
-        	cMakeCache_paths = map(os.path.normpath, glob.glob(self.cMake_binary_dir + "/../*/CMakeCache.txt"))
-        	return None if len(cMakeCache_paths) == 0 else cMakeCache_paths[0]
-
-	def get_cMakeCache_copy_path(self):
-		(l, m, r) = self.stats_cache.rpartition(".")
-		l = l + "_CMakeCache"
-        	return os.path.normpath("".join([l, m, r]))
+    def get_cMakeCache_copy_path(self):
+        (l, m, r) = self.stats_cache.rpartition(".")
+        l = l + "_CMakeCache"
+        return os.path.normpath("".join([l, m, r]))
