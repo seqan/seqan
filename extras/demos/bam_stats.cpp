eeb21d421a7a651ddd01919cb1ad006df1583275
jsinger <jsinger@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-04-24 08:52:35 +0000

    [INTERNAL] Set the right copy right year.
    git-svn-id: http://svn.seqan.de/seqan/trunk@13950 e6417c60-b987-48fd-844e-b20f0fcc1017

--- extras/demos/bam_stats.cpp
+++ extras/demos/bam_stats.cpp
@@ -1,7 +1,7 @@
 // ==========================================================================
 //                                 bam_stats
 // ==========================================================================
-// Copyright (c) 2006-2010, Knut Reinert, FU Berlin
+// Copyright (c) 2006-2013, Knut Reinert, FU Berlin
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
