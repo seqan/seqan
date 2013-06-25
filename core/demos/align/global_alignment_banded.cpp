4e4742e995daf62ba7aafed73bb9c7e20cd358f5
rmaerker <rmaerker@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-06-25 08:04:56 +0000

    [FIX] Fixed compile and runtime problems with the demo file.
    git-svn-id: http://svn.seqan.de/seqan/trunk@14162 e6417c60-b987-48fd-844e-b20f0fcc1017

--- core/demos/align/global_alignment_banded.cpp
+++ core/demos/align/global_alignment_banded.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-#include <seqan/base.h>
+#include <seqan/basic.h>
 #include <seqan/align.h>
+#include <seqan/sequence.h>
 #include <seqan/file.h>  // for printint strings
 
 int main()
@@ -9,21 +10,22 @@ int main()
     seqan::Dna5String seqH = "CGATT";
     seqan::Dna5String seqV = "CGAAATT";
 
-    seqan::Align<Dna5String> align;
+    seqan::Align<seqan::Dna5String> align;
     resize(rows(align), 2);
     assignSource(row(align, 0), seqH);
-    assignSource(row(align, 0), seqV);
+    assignSource(row(align, 1), seqV);
 
     seqan::Score<int, seqan::Simple> scoringScheme(2, -1, -2);
     seqan::AlignConfig<> alignConfig;
 
     int lDiag = -2;
-    int uDiag = -2;
+    int uDiag = 2;
 
     int result = globalAlignment(align, scoringScheme, alignConfig, lDiag, uDiag);
-
+    
+    std::cerr << "Score: " << result << std::endl;
     std::cerr << "The resulting alignment is\n"
-              << align;
+              << align << std::endl;
 
     return 0;
 }
