afa65853b258e88fef053411a3dea026c2149c85
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-07-09 13:19:20 +0000

    [FEATURE] Small test program to check paths.
    Compile against the SeqAn package to check that all paths are available.
    
    
    git-svn-id: http://svn.seqan.de/seqan/trunk@14246 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ misc/include_check.cpp
@@ -0,0 +1,80 @@
+// ==========================================================================
+//                 SeqAn - The Library for Sequence Analysis
+// ==========================================================================
+// Copyright (c) 2006-2013, Knut Reinert, FU Berlin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Knut Reinert or the FU Berlin nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL KNUT REINERT OR THE FU BERLIN BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// ==========================================================================
+// Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
+// ==========================================================================
+// Small program that includes all of SeqAn to check that there are no
+// dependencies on the SeqAn source layout.
+// ==========================================================================
+
+// core modules
+#include <seqan/align.h>
+#include <seqan/arg_parse.h>
+#include <seqan/bam_io.h>
+#include <seqan/basic.h>
+#include <seqan/consensus.h>
+#include <seqan/file.h>
+#include <seqan/find.h>
+#include <seqan/gff_io.h>
+#include <seqan/graph_algorithms.h>
+#include <seqan/graph_msa.h>
+#include <seqan/graph_types.h>
+#include <seqan/index.h>
+#include <seqan/map.h>
+#include <seqan/modifier.h>
+#include <seqan/pipe.h>
+#include <seqan/platform.h>
+#include <seqan/random.h>
+#include <seqan/graph_align.h>
+#include <seqan/score.h>
+#include <seqan/seeds.h>
+#include <seqan/seq_io.h>
+#include <seqan/sequence.h>
+#include <seqan/sequence_journaled.h>
+#include <seqan/store.h>
+#include <seqan/stream.h>
+#include <seqan/system.h>
+#include <seqan/version.h>
+
+// extras modules
+#include <seqan/alignment_free.h>
+#include <seqan/find_motif.h>
+#include <seqan/gff_io.h>
+#include <seqan/journaled_set.h>
+#include <seqan/math.h>
+#include <seqan/parse_lm.h>
+#include <seqan/statistics.h>
+#include <seqan/vcf_io.h>
+
+int main()
+{
+    return 0;
+}
