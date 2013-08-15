0485378edb117b9002213150dbff0855327518fc
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-08-15 09:14:33 +0000

    [DOC] Adding image directory from docs2.
    git-svn-id: http://svn.seqan.de/seqan/trunk@14401 e6417c60-b987-48fd-844e-b20f0fcc1017

--- dox/dox_only.sh
+++ dox/dox_only.sh
@@ -2,4 +2,4 @@
 
 # Run doxygen-style documentation system.
 
-rm -f dddoc_cache.bin && ../util/bin/dox.py -b ../core -b ../extras -i ../core/include/seqan  -i ../extras/include/seqan -i pages
+rm -f dddoc_cache.bin && ../util/bin/dox.py -b ../core -b ../extras -i ../core/include/seqan  -i ../extras/include/seqan -i pages --image-dir ../docs2/images/
