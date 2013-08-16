0f355765752ba9c6f0fd01cdbe63fe2f5c78062f
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-08-16 10:55:08 +0000

    [FEATURE] Small demo package fx_tools that contains NGS data utilities.
    git-svn-id: http://svn.seqan.de/seqan/trunk@14425 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ extras/apps/fx_tools/fx_bam_coverage.cpp
@@ -0,0 +1,315 @@
+// ==========================================================================
+//                               FX Tools
+// ==========================================================================
+// Copyright (c) 2006-2012, Knut Reinert, FU Berlin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Knut Reinert or the FU Berlin nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL KNUT REINERT OR THE FU BERLIN BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// ==========================================================================
+// Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
+// ==========================================================================
+// Computes coverage and C+G content of a genome given a SAM or BAM file,
+// window-based.
+// ==========================================================================
+
+#include <sstream>
+
+#include <seqan/arg_parse.h>
+#include <seqan/basic.h>
+#include <seqan/modifier.h>
+#include <seqan/sequence.h>
+#include <seqan/stream.h>
+#include <seqan/bam_io.h>
+#include <seqan/seq_io.h>
+
+// --------------------------------------------------------------------------
+// Class FxBamCoverageOptions
+// --------------------------------------------------------------------------
+
+struct FxBamCoverageOptions
+{
+    // Verbosity level.  0 - quiet, 1 - normal, 2 - verbose, 3 - very verbose.
+    int verbosity;
+    
+    // Path to Genome file.
+    seqan::CharString inGenomePath;
+
+    // Path to SAM file.
+    seqan::CharString inBamPath;
+
+    // Path to output file.
+    seqan::CharString outPath;
+
+    // Window size to use for computation.
+    __int32 windowSize;
+
+    FxBamCoverageOptions() : verbosity(1), windowSize(10*1000)
+    {}
+};
+
+
+// --------------------------------------------------------------------------
+// Class BinData
+// --------------------------------------------------------------------------
+
+struct BinData
+{
+    // Number of reads whose alignment starts here.
+    unsigned coverage;
+    // Length of underlying sequence.
+    unsigned length;
+    // Percentag C+G.
+    double cgContent;
+    
+    BinData() : coverage(0), length(0), cgContent(0)
+    {}
+};
+
+// --------------------------------------------------------------------------
+// Function parseArgs()
+// --------------------------------------------------------------------------
+
+seqan::ArgumentParser::ParseResult
+parseArgs(FxBamCoverageOptions & options,
+          int argc,
+          char const ** argv)
+{
+    seqan::ArgumentParser parser("fx_bam_coverage");
+    setShortDescription(parser, "Read Coverage Computation.");
+    setVersion(parser, "0.2");
+    setDate(parser, "August 2013");
+    
+    addUsageLine(parser,
+                 "[\\fIOPTIONS\\fP] \\fB-o\\fP \\fIOUT.bam.coverage.tsv\\fP \\fB-r\\fP \\fIGENOME.fa\\fP "
+                 "\\fB-m\\fP \\fIMAPPING.bam\\fP");
+    addDescription(parser, "Compute read coverage and C+G content for a genome.");
+
+    // Two input files: Genome, and mapping.
+    addOption(parser, seqan::ArgParseOption("r", "in-reference", "Path to the reference file.",
+                                            seqan::ArgParseArgument::INPUTFILE, "IN.fa"));
+    setValidValues(parser, "in-reference", "fasta fa");
+    setRequired(parser, "in-reference");
+
+    addOption(parser, seqan::ArgParseOption("m", "in-mapping", "Path to the mapping file to analyze.",
+                                            seqan::ArgParseArgument::INPUTFILE));
+    setValidValues(parser, "in-mapping", "sam bam");
+    setRequired(parser, "in-mapping");
+
+    // TODO(holtgrew): I want a custom help text!
+    // addOption(parser, seqan::ArgParseOption("h", "help", "This helpful screen."));
+    addOption(parser, seqan::ArgParseOption("v", "verbose", "Verbose, log to STDERR."));
+    hideOption(parser, "verbose");
+    addOption(parser, seqan::ArgParseOption("vv", "very-verbose", "Very verbose, log to STDERR."));
+    hideOption(parser, "very-verbose");
+
+    addSection(parser, "Main Options");
+    addOption(parser, seqan::ArgParseOption("w", "window-size", "Set the size of the non-overlapping windows in base pairs.", seqan::ArgParseArgument::INTEGER, "NUM"));
+    setDefaultValue(parser, "window-size", "10000");
+
+    addSection(parser, "Output Options");
+    addOption(parser, seqan::ArgParseOption("o", "out-path", "Path to the resulting file.  If omitted, result is printed to stdout.", seqan::ArgParseArgument::OUTPUTFILE, "TSV"));
+    setRequired(parser, "out-path");
+    setValidValues(parser, "out-path", "bam.coverage.tsv");
+
+    seqan::ArgumentParser::ParseResult res = parse(parser, argc, argv);
+
+    if (res == seqan::ArgumentParser::PARSE_OK)
+    {
+        getOptionValue(options.inGenomePath, parser, "in-reference");
+        getOptionValue(options.inBamPath, parser, "in-mapping");
+        getOptionValue(options.outPath, parser, "out-path");
+
+        if (isSet(parser, "verbose"))
+            options.verbosity = 2;
+        if (isSet(parser, "very-verbose"))
+            options.verbosity = 3;
+    }
+
+    return res;
+}
+
+// ---------------------------------------------------------------------------
+// Function main()
+// ---------------------------------------------------------------------------
+
+int main(int argc, char const ** argv)
+{
+    double startTime = seqan::sysTime();
+    
+    // -----------------------------------------------------------------------
+    // Parse command line.
+    // -----------------------------------------------------------------------
+    FxBamCoverageOptions options;
+    seqan::ArgumentParser::ParseResult res = parseArgs(options, argc, argv);
+    if (res != seqan::ArgumentParser::PARSE_OK)
+        return res == seqan::ArgumentParser::PARSE_ERROR;  // 1 on errors, 0 otherwise
+
+    // -----------------------------------------------------------------------
+    // Show options.
+    // -----------------------------------------------------------------------
+    if (options.verbosity >= 1)
+    {
+        std::cerr << "____OPTIONS___________________________________________________________________\n"
+                  << "\n"
+                  << "VERBOSITY    " << options.verbosity << "\n"
+                  << "GENOME       " << options.inGenomePath << "\n"
+                  << "SAM/BAM      " << options.inBamPath << "\n"
+                  << "OUT          " << options.outPath << "\n"
+                  << "WINDOW SIZE  " << options.windowSize << "\n";
+    }
+
+    // -----------------------------------------------------------------------
+    // Load Genome FAI Index
+    // -----------------------------------------------------------------------
+
+    std::cerr << "\n"
+              << "___PREPRATION_____________________________________________________________________\n"
+              << "\n"
+              << "Indexing GENOME file  " << options.inGenomePath << " ...";
+    seqan::FaiIndex faiIndex;
+    if (build(faiIndex, toCString(options.inGenomePath)) != 0)
+    {
+        std::cerr << "Could not build FAI index.\n";
+        return 1;
+    }
+    std::cerr << " OK\n";
+
+    // Prepare bins.
+    seqan::String<seqan::String<BinData> > bins;
+    resize(bins, numSeqs(faiIndex));
+
+    // -----------------------------------------------------------------------
+    // Compute C+G content 
+    // -----------------------------------------------------------------------
+
+    std::cerr << "\n"
+              << "___C+G CONTENT COMPUTATION________________________________________________________\n"
+              << "\n";
+
+    for (unsigned i = 0; i < numSeqs(faiIndex); ++i)
+    {
+        std::cerr << "[" << sequenceName(faiIndex, i) << "] ...";
+        unsigned numBins = (sequenceLength(faiIndex, i) + options.windowSize - 1) / options.windowSize;
+        resize(bins[i], numBins);
+        seqan::Dna5String contigSeq;
+        if (readSequence(contigSeq, faiIndex, i) != 0)
+        {
+            std::cerr << "\nERROR: Could not read sequence " << sequenceName(faiIndex, i) << " from file!\n";
+            return 1;
+        }
+
+        for (unsigned bin = 0; bin < numBins; ++bin)
+        {
+            unsigned cgCounter = 0;
+            unsigned binSize = 0;
+            bins[i][bin].length = options.windowSize;
+            if ((bin + 1) * options.windowSize > length(contigSeq))
+                bins[i][bin].length = length(contigSeq) - bin * options.windowSize;
+            for (unsigned pos = bin * options.windowSize; pos < length(contigSeq) && pos < (bin + 1) * options.windowSize; ++pos, ++binSize)
+                cgCounter += (contigSeq[pos] == 'C' || contigSeq[pos] == 'G');
+            bins[i][bin].cgContent = 1.0 * cgCounter / binSize;
+        }
+        std::cerr << "DONE\n";
+    }
+
+    // -----------------------------------------------------------------------
+    // Compute Coverage
+    // -----------------------------------------------------------------------
+
+    std::cerr << "\n"
+              << "___COVERAGE COMPUATATION________________________________________________________\n"
+              << "\n"
+              << "Computing Coverage...";
+
+    seqan::BamStream bamStream(toCString(options.inBamPath));
+    if (!isGood(bamStream))
+    {
+        std::cerr << "Could not open " << options.inBamPath << "!\n";
+        return 1;
+    }
+
+    seqan::BamAlignmentRecord record;
+    while (!atEnd(bamStream))
+    {
+        if (readRecord(record, bamStream) != 0)
+        {
+            std::cerr << "ERROR: Could not read record from BAM file!\n";
+            return 1;
+        }
+
+        if (hasFlagUnmapped(record) || hasFlagSecondary(record) || record.rID == seqan::BamAlignmentRecord::INVALID_REFID)
+            continue;  // Skip these records.
+
+        int contigId = 0;
+        seqan::CharString const & contigName = nameStore(bamStream.bamIOContext)[record.rID];
+        if (!getIdByName(faiIndex, contigName, contigId))
+        {
+            std::cerr << "ERROR: Alignment to unknown contig " << contigId << "!\n";
+            return 1;
+        }
+        unsigned binNo = record.beginPos / options.windowSize;
+        bins[contigId][binNo].coverage += 1;
+    }
+
+    std::cerr << "DONE\n";
+
+    // -----------------------------------------------------------------------
+    // Write Output
+    // -----------------------------------------------------------------------
+
+    std::ostream * out = &std::cout;
+    std::ofstream outFile;
+    if (options.outPath != "-")
+    {
+        outFile.open(toCString(options.outPath), std::ios::binary | std::ios::out);
+        if (!outFile.good())
+        {
+            std::cerr << "ERROR: Could not open output file " << options.outPath << "!\n";
+            return 1;
+        }
+        out = &outFile;
+    }
+
+    (*out) << "#BIN\tREF_NAME\tREF_BIN\tBIN_BEGIN\tBIN_LENGTH\tCOVERAGE\tCG_CONTENT\n";
+    for (unsigned i = 0, globalBin = 0; i < length(bins); ++i)
+    {
+        for (unsigned refBin = 0; refBin < length(bins[i]); ++refBin, ++globalBin)
+        {
+            (*out) << globalBin << '\t'
+                   << sequenceName(faiIndex, i) << '\t'
+                   << refBin << '\t'
+                   << refBin * options.windowSize << '\t'
+                   << bins[i][refBin].length << '\t'
+                   << bins[i][refBin].coverage << '\t'
+                   << bins[i][refBin].cgContent << '\n';
+        }
+    }
+
+    if (options.verbosity >= 2)
+        std::cerr << "Took " << (seqan::sysTime() - startTime) << " s\n";
+
+    return 0;
+}
---
+++ extras/apps/fx_tools/fx_fastq_stats.cpp
@@ -0,0 +1,354 @@
+// ==========================================================================
+//                                 FX Tools
+// ==========================================================================
+// Copyright (c) 2006-2012, Knut Reinert, FU Berlin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Knut Reinert or the FU Berlin nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL KNUT REINERT OR THE FU BERLIN BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// ==========================================================================
+// Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
+// ==========================================================================
+
+#include <map>
+
+#include <seqan/arg_parse.h>
+#include <seqan/basic.h>
+#include <seqan/seq_io.h>
+#include <seqan/sequence.h>
+
+// ==========================================================================
+// Classes
+// ==========================================================================
+
+// --------------------------------------------------------------------------
+// Class AppOptions
+// --------------------------------------------------------------------------
+
+// This struct stores the options from the command line.
+//
+// You might want to rename this to reflect the name of your app.
+
+struct AppOptions
+{
+    // Verbosity level.  0 -- quiet, 1 -- normal, 2 -- verbose, 3 -- very verbose.
+    int verbosity;
+
+    // The input file name is a string.
+    seqan::CharString inFilename;
+
+    // The out file name is an out file.
+    seqan::CharString outFilename;
+
+    AppOptions() :
+        verbosity(1)
+    {}
+};
+
+// --------------------------------------------------------------------------
+// Class FastqStats
+// --------------------------------------------------------------------------
+
+struct FastqStats
+{
+    // -----------------------------------------------------------------------
+    // Members with Results
+    // -----------------------------------------------------------------------
+
+    unsigned maxLength;
+    
+    // Number of bases in column i.
+    seqan::String<__int64> numBases;
+    // Smallest score in column i.
+    seqan::String<__int32> minScores;
+    // Largest score in column i.
+    seqan::String<__int32> maxScores;
+    // Sum of scores in column i.
+    seqan::String<__int64> sumScores;
+    // Mean of scores in column i.
+    seqan::String<double> meanScores;
+    // First quartile quality score (Q1) for column i.
+    seqan::String<double> firstQuartiles;
+    // Median quality score for column i.
+    seqan::String<double> medianScores;
+    // Third quartile quality score (Q3) for column i.
+    seqan::String<double> thirdQuartiles;
+    // Inter-quartile range  (Q3-Q1) for column i.
+    seqan::String<double> interQuartileRanges;
+    // Left-whisker value for boxplotting for column i.
+    seqan::String<__int32> leftWhiskers;
+    // Right-whisker value for boxplotting for column i.
+    seqan::String<__int32> rightWhiskers;
+    // Number of nucleotides A, C, G, T, N for column i.
+    seqan::String<seqan::String<__int64> > nucleotideCounts;
+
+    // -----------------------------------------------------------------------
+    // Histogram Members
+    // -----------------------------------------------------------------------
+
+    // Quality histogram.
+    seqan::String<std::map<__int32, __int32> > qualHistos;
+
+    // -----------------------------------------------------------------------
+    // Constructor
+    // -----------------------------------------------------------------------
+
+    FastqStats() : maxLength(0)
+    {}
+
+    // -----------------------------------------------------------------------
+    // Member Functions
+    // -----------------------------------------------------------------------
+
+    // Resize members to read of length.
+    void resizeToReadLength(unsigned n)
+    {
+        if (maxLength == n)
+            return;
+        maxLength = n;
+
+        resize(numBases, n, 0);
+        resize(minScores, n, 0);
+        resize(maxScores, n, 0);
+        resize(sumScores, n, 0);
+        resize(meanScores, n, 0);
+        resize(firstQuartiles, n, 0);
+        resize(medianScores, n, 0);
+        resize(thirdQuartiles, n, 0);
+        resize(interQuartileRanges, n, 0);
+        resize(leftWhiskers, n, 0);
+        resize(rightWhiskers, n, 0);
+        resize(nucleotideCounts, n);
+        for (unsigned i = 0; i < n; ++i)
+            resize(nucleotideCounts[i], 5, 0);
+
+        resize(qualHistos, n);
+    }
+
+    // Update histogram and statistics for the given read.
+    void registerRead(seqan::Dna5String const & seq, seqan::CharString const & quals)
+    {
+        resizeToReadLength(length(seq));
+
+        // Update nucleotide counts.
+        for (unsigned i = 0; i < length(seq); ++i)
+        {
+            numBases[i] += 1;
+            nucleotideCounts[i][ordValue(seq[i])] += 1;
+        }
+
+        // Update quality histograms and statistics.
+        for (unsigned i = 0; i < length(quals); ++i)
+        {
+            int qual = quals[i] - '!';  // PHRED scores.
+            if (numBases[i] == 0u || minScores[i] > qual)
+                minScores[i] = qual;
+            if (numBases[i] == 0u || maxScores[i] < qual)
+                maxScores[i] = qual;
+            qualHistos[i][qual] += 1;
+            sumScores[i] += qual;
+        }
+    }
+
+    // Compute statistics after updating for the last read.
+    void finalizeStats()
+    {
+        // Compute score means.
+        for (unsigned i = 0; i < length(meanScores); ++i)
+            meanScores[i] = (1.0 * sumScores[i]) / numBases[i];
+        // Compute score medians and quartiles.
+        for (unsigned i = 0; i < length(qualHistos); ++i)
+        {
+            if (qualHistos[i].size() == 0u)
+                continue;  // Skip if empty.
+
+            // TODO(holtgrew): Quartile/median computation not mathematically correct yet.
+            unsigned n = numBases[i];  // Number of bases.
+            unsigned firstQN = n / 4;
+            unsigned medianN = n / 2;
+            unsigned thirdQN = (3 * n) / 4;
+            unsigned count = 0;  // Number of bases up to here.
+            for (std::map<__int32, __int32>::const_iterator it = qualHistos[i].begin(); it != qualHistos[i].end(); ++it)
+            {
+                if (count < firstQN && count + it->second >= firstQN)
+                    firstQuartiles[i] = it->first;
+                if (count < medianN && count + it->second >= medianN)
+                    medianScores[i] = it->first;
+                if (count < thirdQN && count + it->second >= thirdQN)
+                    thirdQuartiles[i] = it->first;
+
+                count += it->second;
+            }
+            interQuartileRanges[i] = (thirdQuartiles[i] - firstQuartiles[i]);
+        }
+
+        // Compute whiskers as the data point that is still within 1.5 IQR of the lower quartile.
+        for (unsigned i = 0; i < length(qualHistos); ++i)
+        {
+            if (qualHistos[i].size() == 0u)
+                continue;  // Skip if empty.
+            leftWhiskers[i] = firstQuartiles[i];
+            rightWhiskers[i] = thirdQuartiles[i];
+            double leftWhiskerBound = ((double)firstQuartiles[i]) - 1.5 * interQuartileRanges[i];
+            double rightWhiskerBound = ((double)thirdQuartiles[i]) + 1.5 * interQuartileRanges[i];
+            for (std::map<__int32, __int32>::const_iterator it = qualHistos[i].begin(); it != qualHistos[i].end(); ++it)
+                if (leftWhiskers[i] > it->first && it->first >= leftWhiskerBound)
+                    leftWhiskers[i] = it->first;
+            for (std::map<__int32, __int32>::const_iterator it = qualHistos[i].begin(); it != qualHistos[i].end(); ++it)
+                if (rightWhiskers[i] < it->first && it->first <= rightWhiskerBound)
+                    rightWhiskers[i] = it->first;
+        }
+    }
+};
+
+// ==========================================================================
+// Functions
+// ==========================================================================
+
+// --------------------------------------------------------------------------
+// Function parseCommandLine()
+// --------------------------------------------------------------------------
+
+seqan::ArgumentParser::ParseResult
+parseCommandLine(AppOptions & options, int argc, char const ** argv)
+{
+    // Setup ArgumentParser.
+    seqan::ArgumentParser parser("fx_fastq_stats");
+    // Set short description, version, and date.
+    setShortDescription(parser, "Compute FASTQ statistics.");
+    setCategory(parser, "NGS Quality Control");
+    setVersion(parser, "0.1");
+    setDate(parser, "August 2012");
+
+    // Define usage line and long description.
+    addUsageLine(parser, "\\fB-i\\fP \\fIINPUT.fq\\fP \\fB-o\\fP \\fIOUTPUT.fq.stats.tsv\\fP");
+    addDescription(parser,
+                   "Read a FASTQ file with reads sequences of the same length.  Writes out a TSV file with one record for "
+                   "each column/position with statistics on the nucleotides and qualities.");
+
+    addSection(parser, "Input / Output");
+    addOption(parser, seqan::ArgParseOption("i", "input", "Input FASTQ file.", seqan::ArgParseOption::INPUTFILE, "INPUT"));
+    setValidValues(parser, "input", "fastq fq");
+    setRequired(parser, "input");
+    addOption(parser, seqan::ArgParseOption("o", "output", "Output TSV file.", seqan::ArgParseOption::OUTPUTFILE, "OUTPUT"));
+    setRequired(parser, "output");
+    setValidValues(parser, "output", ".fq.stats.tsv");
+
+    // Parse command line.
+    seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
+
+    // Only extract  options if the program will continue after parseCommandLine()
+    if (res != seqan::ArgumentParser::PARSE_OK)
+        return res;
+
+    seqan::getOptionValue(options.inFilename, parser, "input");
+    seqan::getOptionValue(options.outFilename, parser, "output");
+
+    return seqan::ArgumentParser::PARSE_OK;
+}
+
+// --------------------------------------------------------------------------
+// Function main()
+// --------------------------------------------------------------------------
+
+int main(int argc, char const ** argv)
+{
+    // Parse the command line.
+    seqan::ArgumentParser parser;
+    AppOptions options;
+    seqan::ArgumentParser::ParseResult res = parseCommandLine(options, argc, argv);
+    // If there was an error parsing or built-in argument parser functionality
+    // was triggered then we exit the program.  The return code is 1 if there
+    // were errors and 0 if there were none.
+    if (res != seqan::ArgumentParser::PARSE_OK)
+        return res == seqan::ArgumentParser::PARSE_ERROR;
+
+    // Open input file.
+    seqan::SequenceStream inStream(toCString(options.inFilename));
+    if (!isGood(inStream))
+    {
+        std::cerr << "ERROR: Could not open file " << options.inFilename << " for reading.\n";
+        return 1;
+    }
+
+    // Read sequences and build result.
+    FastqStats stats;
+    seqan::CharString id;
+    seqan::Dna5String seq;
+    seqan::CharString quals;
+    while (!atEnd(inStream))
+    {
+        if (readRecord(id, seq, quals, inStream) != 0)
+        {
+            std::cerr << "ERROR: Could not read from " << options.inFilename << ".\n";
+            return 1;
+        }
+        if (empty(quals))  // Fill with Q40 if there are no qualities.
+            resize(quals, length(seq), '!' + 40);
+
+        // Update statistics.
+        stats.registerRead(seq, quals);
+    }
+
+    // Finalize statistics and write to output.
+    stats.finalizeStats();
+
+    std::ostream * out = &std::cout;
+    std::ofstream outStream;
+    if (options.outFilename != "-")
+    {
+        outStream.open(toCString(options.outFilename), std::ios::binary | std::ios::out);
+        if (!outStream.good())
+        {
+            std::cerr << "ERROR: Could not open file " << options.outFilename << " for writing.\n";
+            return 1;
+        }
+        out = &outStream;
+    }
+
+    *out << "#column\tcount\tmin\tmax\tsum\tmean\tQ1\tmedian\tQ3\tIQR\tlW\trW\tA_count\tC_count\tG_count\tT_count\tN_count\n";
+    for (unsigned i = 0; i < stats.maxLength; ++i)
+    {
+        *out << i << "\t"
+             << stats.numBases[i] << "\t"
+             << stats.minScores[i] << "\t"
+             << stats.maxScores[i] << "\t"
+             << stats.sumScores[i] << "\t"
+             << stats.meanScores[i] << "\t"
+             << stats.firstQuartiles[i] << "\t"
+             << stats.medianScores[i] << "\t"
+             << stats.thirdQuartiles[i] << "\t"
+             << stats.interQuartileRanges[i] << "\t"
+             << stats.leftWhiskers[i] << "\t"
+             << stats.rightWhiskers[i] << "\t"
+             << stats.nucleotideCounts[i][0] << "\t"
+             << stats.nucleotideCounts[i][1] << "\t"
+             << stats.nucleotideCounts[i][2] << "\t"
+             << stats.nucleotideCounts[i][3] << "\t"
+             << stats.nucleotideCounts[i][4] << "\n";
+    }
+
+    return 0;
+}
