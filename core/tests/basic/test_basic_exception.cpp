45aef746fe8cabcdf89a6d5b8e4a1d4092a59ecc
esiragusa <esiragusa@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-09-30 14:17:47 +0000

    [TESTS] Exceptions: added basic exceptions tests
    git-svn-id: http://svn.seqan.de/seqan/trunk@14588 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ core/tests/basic/test_basic_exception.cpp
@@ -0,0 +1,48 @@
+// ==========================================================================
+//                 SeqAn - The Library for Sequence Analysis
+// ==========================================================================
+// Copyright (c) 2006-2013, Knut Reinert, FU Berlin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Knut Reinert or the FU Berlin nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL KNUT REINERT OR THE FU BERLIN BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// ==========================================================================
+// Author: Enrico Siragusa <enrico.siragusa@fu-berlin.de>
+// ==========================================================================
+// Tests for the sub module basic_exception.
+// ==========================================================================
+
+#include <seqan/basic/basic_debug.h>
+#include <seqan/basic/basic_exception.h>
+
+#include "test_basic_exception.h"
+
+SEQAN_BEGIN_TESTSUITE(test_basic_exception)
+{
+    SEQAN_CALL_TEST(test_basic_exception_try_catch);
+    SEQAN_CALL_TEST(test_basic_exception_bad_alloc);
+    SEQAN_CALL_TEST(test_basic_exception_runtime_error);
+}
+SEQAN_END_TESTSUITE
