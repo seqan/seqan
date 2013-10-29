0afe2478f30a7f0799348f3ab81b7d833cd29efa
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2013-10-29 15:16:16 +0100

    [INTERNAL] SequenceFile: updated writing demo

--- core/demos/seq_io/sequence_stream_write.cpp
+++ core/demos/seq_io/sequence_stream_write.cpp
@@ -1,6 +1,6 @@
 #include <seqan/basic.h>
-#include <seqan/seq_io.h>
 #include <seqan/sequence.h>
+#include <seqan/seq_io.h>
 
 using namespace seqan;
 
@@ -9,21 +9,14 @@ int main(int argc, char ** argv)
     CharString path = SEQAN_TEMP_FILENAME();
     append(path, ".fa");
 
-    // Open file and check for errors.
-    SequenceStream seqStream(toCString(path), SequenceStream::WRITE);
-    if (!isGood(seqStream))
-    {
-        std::cerr << "ERROR: Could not open " << path << " for writing.\n";
-        return 1;
-    }
+    SequenceFile<Output> file(toCString(path));
 
-    // Write two sequences to the file.
-    if (writeRecord(seqStream, "one", "CGAT") != 0 ||
-        writeRecord(seqStream, "two", "ASDF") != 0)
-    {
-        std::cerr << "ERROR: Problem writing to " << path << "\n";
-        return 1;
-    }
+    CharString meta = "chr1";
+    CharString seq = "ACGT";
+
+    write(file, meta, seq);
+
+    close(file);
 
     return 0;
 }
