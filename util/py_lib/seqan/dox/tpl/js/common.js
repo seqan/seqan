5e5ca5d304fff86fdb03128efcc56cc92442eb72
bjornson <bjornson@e6417c60-b987-48fd-844e-b20f0fcc1017>
2013-11-11 15:06:24 +0000

    Added SUA data mining code
    git-svn-id: http://svn.seqan.de/seqan/trunk@14727 e6417c60-b987-48fd-844e-b20f0fcc1017

--- util/py_lib/seqan/dox/tpl/js/common.js
+++ util/py_lib/seqan/dox/tpl/js/common.js
@@ -24,16 +24,7 @@
         $('[title]:not([href])').tooltip({ container: 'body' });
 
         // smooth scrolling
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                    $('html,body').animate({ scrollTop: target.offset().top-20 }, 350);
-                    return false;
-                }
-            }
-        });
+        $('a[href*=#]:not([href=#])').smoothScroll({ offset: -20 });
 
     });
 
