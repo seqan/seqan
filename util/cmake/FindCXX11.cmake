8066a18511cc4d7635ad410923a4fcaa9b82f43b
David Weese <dave.weese@googlemail.com>
2013-11-26 18:01:16 +0100

    [FIX] added C++11 cmake module

---
+++ util/cmake/FindCXX11.cmake
@@ -0,0 +1,32 @@
+if(__FIND_CXX11_CMAKE__)
+  return()
+endif()
+set(__FIND_CXX11_CMAKE__ TRUE)
+
+if (MSVC AND MSVC_VERSION GREATER 1400)
+  set(CXX11_FOUND 1)
+  return ()
+endif (MSVC AND MSVC_VERSION GREATER 1400)
+
+include(CheckCXXCompilerFlag)
+enable_language(CXX)
+
+check_cxx_compiler_flag("-std=c++11" CXX11_FOUND)
+if (CXX11_FOUND)
+  set (CXX11_CXX_FLAGS "-std=c++11")
+
+  # Tested on Mac OS X 10.8.2 with XCode 4.6 Command Line Tools
+  # Clang requires this to find the correct c++11 headers
+  if (CMAKE_HOST_APPLE)
+    set (CXX11_CXX_FLAGS "${CXX11_CXX_FLAGS} -stdlib=libc++")
+  endif (CMAKE_HOST_APPLE)
+
+else (CXX11_FOUND)
+
+  check_cxx_compiler_flag("-std=c++0x" CXX11_FOUND)
+  if (CXX11_FOUND)
+    set (CXX11_CXX_FLAGS "-std=c++0x")
+  endif (CXX11_FOUND)
+
+endif (CXX11_FOUND)
+
