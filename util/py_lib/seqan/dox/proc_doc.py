ff6fe5e1b39ae0fe20f7c34ab0eaa6c22712a3b8
Manuel Holtgrewe <zyklenfrei@googlemail.com>
2013-12-21 14:49:24 +0100

    [DOC,INTERNAL] Not crashing in case of double definition, only warn and ignore.

--- util/py_lib/seqan/dox/proc_doc.py
+++ util/py_lib/seqan/dox/proc_doc.py
@@ -127,9 +127,11 @@ class ProcDoc(object):
                 'old_name': old.name,
                 'old_file' : x.location[0],
                 'old_line' : x.location[1]}
-            raise DocumentationBuildException(token=x.raw_entry.name.tokens[0], msg=tpl % vals)
-        self.entries[name] = x
-        x.doc = self
+            self.doc_processor.msg_printer.printTokenError(x.raw_entry.name.tokens[0],
+                                                           tpl % vals, 'error')
+        else:
+          self.entries[name] = x
+          x.doc = self
 
     def runTextVisitor(self, v):
         """Run visitor v on all Text members of all entries and sub entries.
