d7118c110fcc395b7e5a27a8acbb9b3847516559
David Weese <dave.weese@googlemail.com>
2014-02-03 23:32:47 +0100

    [FIX] fixed permissions

--- util/travis/linux-cibuild.before.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# install required packages
-sudo apt-get install -qq cmake \
-                         python \
-                         zlib1g-dev \
-                         libbz2-dev \
-                         libboost-dev
