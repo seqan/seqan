07579323c2e04811867b3da1e66e913cd379cadc
unknown <seqan_dev@Margerite.imp.fu-berlin.de>
2014-05-01 12:25:22 +0200

    [FIX] removed VS12 ambigous bug

--- core/include/seqan/map/map_base.h
+++ core/include/seqan/map/map_base.h
@@ -39,10 +39,17 @@
 namespace SEQAN_NAMESPACE_MAIN
 {
 
-// Manual Forwards
+// ==========================================================================
+// Forwards
+// ==========================================================================
+
+#if !defined(_MSC_VER) || _MSC_VER < 12
+
 template <typename TKey, typename TCargo, typename TCompare, typename TAlloc, typename TKey2>
 inline typename Cargo< ::std::map<TKey,TCargo, TCompare, TAlloc> >::Type &
 cargo(::std::map<TKey,TCargo, TCompare, TAlloc> & me, TKey2 const & _key);
+
+#endif  // #if !defined(_MSC_VER) || _MSC_VER < 12
 
 //////////////////////////////////////////////////////////////////////////////
 //insertion tags
