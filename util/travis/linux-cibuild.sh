7788636e2e452ae33efce89b5f9753a013a3771c
David Weese <dave.weese@googlemail.com>
2014-05-13 14:14:37 +0200

    [FIX] fixed code review comments (+56 squashed commits) Squashed commits: [e43db31] [FIX] removed warning [9424d77] [FIX] reenabled clang [520143b] [FIX] reenabled all tests [6733a01] [FIX] manually install libstdc++-4.8 [2339387] [FIX] full output in ctest [d88c538] [FEATURE] added optimized implementation for fixed-size queues (Queue<.., Limit>) [b0339ef] [FIX] fixed travis [f6d6218] [FIX] clean up, reenabled tests [1dd4b81] [FIX] added pause waits [49bb46a] [FIX] fixed typo if C++11 is not available [f19bbf3] [FEATURE] refactored some spin-loops and added pause instruction [dba0ea1] [FIX] reduced debug output [e28bd34] [DEBUG] output queue throughput [7551aa3] [FIX] warning removed [1931227] [FIX] updated clang in Travis [4035d0b] [FIX] updated clang in Travis [9cb8e6c] [FIX] updated clang in Travis [5c92444] [FIX] updated clang in Travis [ffa184e] [FIX] updated clang in Travis [0ab8f14] [FIX] updated clang in Travis
    Conflicts:
    	.travis.yml
    [9897c92] [FIX] fixed very nasty bug (semicolon between while header and body)
    [7833190] [FIX] added C++11 dependency to parallel test
    [23f986d] [FIX] warnings removed
    [d5631a3] [FIX] fixed non-C++11 compiler issues
    [06227a3] [DEBUG] nailed bug down to CAS problem
    [6d18aa3] [FIX] fixed compare_exchange_weak bug
    [53ba8d8] [FIX] slimmed test
    [cccb9ef] more debug info
    [dc668c1] [FIX] used C++11 atomics
    [f1ef938] [NOP] undo last test
    [cb9d667] [DEBUG] test with memory fence
    [af731ca] [FIX] fixed bug in destructor
    [0c78fdd] [NOP] cosmetics
    [1d48685] [FIX[ more mixed parallelism tests
    [d4c0e25] [FIX] added tests for SPSC schemes
    [998ee99] [NOP] debug
    [c5b14c7] [DEBUG] temporarily disabled all but the parallel test
    
    Conflicts:
    	util/travis/linux-cibuild.sh
    [59f3c65] [NOP] debug infos
    [301607a] [FIX] small fixes
    [7b36660] [DOC] documentation and demo added
    
    Conflicts:
    	core/include/seqan/sequence/sequence_interface.h
    [2165347] [FIX] serial atomicCas to actually check the old value as opposed to serial atomicCasBool
    
    Conflicts:
    	core/tests/index/test_qgram_index.h
    [17432e9] [FIX] fixed wrong forward
    [6d1b509] [FIX] added rvalue semantics for appendValue and general forward/move macros
    
    Conflicts:
    	core/include/seqan/basic/array_construct_destruct.h
    	core/include/seqan/platform.h
    	core/include/seqan/sequence/string_base.h
    	core/include/seqan/sequence/string_block.h
    [64213ea] [FIX] added parallel wrappers for popFront and appendValue
    [d1d422a] [FEATURE] allow to select between SPSC/SPMC/MPSC/MPMC schemes with Serial/Parallel Tags
    [dc3462b] [FIX] fixed ABA problem in MPMC accessor scheme
    [0647d54] [FIX] added atomicCasBool
    
    Conflicts:
    	core/include/seqan/parallel/parallel_atomic_primitives.h
    [418521f] [FIX] added missing barrier for writers and readers
    [74bcbbc] first test with small fixed-size queue
    [94f2f8a] [FIX] fixed some concurrency issues and introduced a popFront() that only if the queue is empty and there is no writer left
    [4162cb1] [FIX] finished serial tests
    [f88cb14] [FIX] made data string a direct member, specialized by TValue
    [a48fc4c] [FIX] C++11 fixes
    
    Conflicts:
    	core/include/seqan/parallel.h
    [47137ec] [FIX] fixed resize of queue
    [157e4ca] [FEATURE] first version of a parallel fifo queue
    [350424c] [NOP] refactored read/write lock into separate header file

---
+++ util/travis/linux-cibuild.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# get some infos from git to embed it in the build name
+export SOURCE_DIRECTORY=`pwd`
+mkdir -p _build
+
+# define the build name
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  export BUILD_NAME=${TRAVIS_PULL_REQUEST}
+elif [[ -n "${TRAVIS_COMMIT_RANGE}" ]]; then
+  export BUILD_NAME=${TRAVIS_COMMIT_RANGE}
+else
+  export BUILD_NAME=${TRAVIS_COMMIT}
+fi
+
+# disable OpenMP warnings for clang
+if [ "${CXX}" == "clang++" ]; then
+  export CXXFLAGS="${CXXFLAGS} -DSEQAN_IGNORE_MISSING_OPENMP=1"
+fi
+
+ctest -V -S util/travis/linux-cibuild.cmake
+
+# we indicate build failures if ctest experienced any errors
+if [ -f ${SOURCE_DIRECTORY}/failed ]; then
+  exit -1
+fi
+
+FAILED_TEST=$(find _build -name "Test.xml" -type f | xargs grep "<Test Status=\"failed\">" -c)
+
+if [ "${FAILED_TEST}" -gt "0" ]; then
+    exit -1
+fi
+
+# it seems like everything worked
+exit 0
