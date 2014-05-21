6d75cdff058d86532a13a80655efbd0c86387d31
David Weese <dave.weese@googlemail.com>
2014-05-21 18:53:40 +0200

    [NOP] debug

--- core/tests/parallel/test_parallel.cpp
+++ core/tests/parallel/test_parallel.cpp
@@ -86,8 +86,8 @@ SEQAN_BEGIN_TESTSUITE(test_parallel) {
 #if defined(_OPENMP)
     if (omp_get_max_threads() >= 2)
     {
-        SEQAN_CALL_TEST(test_parallel_queue_mpmc_fixedsize);
         SEQAN_CALL_TEST(test_parallel_queue_mpmc_dynamicsize);
+        SEQAN_CALL_TEST(test_parallel_queue_mpmc_fixedsize);
     }
 #endif
 }
