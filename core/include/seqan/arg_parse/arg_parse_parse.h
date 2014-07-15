0f0237be3289d6d4409cb8b99ecd4604737cc11e
David Weese <dave.weese@googlemail.com>
2014-07-15 18:26:06 +0200

    [FIX] allow - as input/output file name to represent cin/cout

--- core/include/seqan/arg_parse/arg_parse_parse.h
+++ core/include/seqan/arg_parse/arg_parse_parse.h
@@ -123,7 +123,8 @@ public:
 
         for (int argi = 1; argi < argc; ++argi)
         {
-            if (seenDashDash || strlen(argv[argi]) == 0 || argv[argi][0] != '-')
+            size_t argLen = strlen(argv[argi]);
+            if (seenDashDash || argLen == 0 || (argv[argi][0] != '-' ^ argLen == 1)) //
                 // Handle as position argument if we have seen "--" or does not start with dash.
                 handleArgument(argv[argi]);
             else if (strcmp(argv[argi], "--") == 0)
@@ -341,7 +342,7 @@ inline ArgumentParser::ParseResult parse(ArgumentParser & me,
         printHelp(me, outputStream, format);
         return ArgumentParser::PARSE_EXPORT_HELP;
     }
-    else if (argc == 1 && (me.argumentList.size() > 0 || !_allRequiredSet(me)))
+    else if (argc == 1 && !(_allRequiredSet(me) && _allArgumentsSet(me)))
     {
         // print short help and exit
         printShortHelp(me, errorStream);
