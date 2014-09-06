0adc3ae63731e5d54f12ecbc06984d7a1ab62024
David Weese <dave.weese@googlemail.com>
2014-09-06 17:33:52 +0200

    [FIX] fixed t-coffee I/O and demos

--- core/include/seqan/graph_msa/graph_align_tcoffee_msa.h
+++ core/include/seqan/graph_msa/graph_align_tcoffee_msa.h
@@ -29,6 +29,8 @@
 // DAMAGE.
 //
 // ==========================================================================
+// Author: Tobias Rausch <rausch@embl.de>
+// ==========================================================================
 
 #ifndef SEQAN_HEADER_GRAPH_ALIGN_TCOFFEE_MSA_H
 #define SEQAN_HEADER_GRAPH_ALIGN_TCOFFEE_MSA_H
