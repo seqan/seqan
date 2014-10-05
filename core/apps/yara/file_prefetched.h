b5452e83b40c47a3d8331445e8e889274f14e2dd
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-10-05 20:48:41 +0200

    [API] Yara: PrefetchedFile implements readRecords()

--- core/apps/yara/file_prefetched.h
+++ core/apps/yara/file_prefetched.h
@@ -69,7 +69,7 @@ struct PrefetchedFile
 
     void operator()()
     {
-        readRecords(_toParameter(records), file, maxRecords);
+        readRecords(records, file, maxRecords);
     }
 };
 
@@ -80,20 +80,15 @@ struct PrefetchedFile
 template <typename TFile, typename TRecords>
 struct PrefetchedFile<TFile, TRecords, Parallel>
 {
-    typedef PrefetchedFile<TFile, TRecords *, Serial> TWorker;
+    typedef PrefetchedFile<TFile, TRecords, Serial> TWorker;
 
-    Pair<TRecords>      recordsQueue;
-    TRecords *          records;
     TWorker             _worker;
     Thread<TWorker>     reader;
 
     PrefetchedFile(__uint64 maxRecords) :
-        records(&recordsQueue.i1),
         _worker(maxRecords),
         reader(_worker)
-    {
-        reader.worker.records = &recordsQueue.i2;
-    }
+    {}
 };
 
 // ============================================================================
@@ -170,52 +165,27 @@ inline void close(PrefetchedFile<TFile, TRecords, Parallel> & me)
 // Function readRecords<Serial>()
 // ----------------------------------------------------------------------------
 
-//template <typename TFile, typename TRecords, typename TThreading>
-//inline void readRecords(TRecords & records, PrefetchedFile<TFile, TRecords, TThreading> & me)
-//{
-//    readRecords(records, me.file, me.maxRecords);
-//}
+template <typename TFile, typename TRecords, typename TThreading>
+inline void readRecords(TRecords & records, PrefetchedFile<TFile, TRecords, TThreading> & me)
+{
+    readRecords(records, me.file, me.maxRecords);
+}
 
 // ----------------------------------------------------------------------------
 // Function readRecords<Parallel>()
 // ----------------------------------------------------------------------------
 
-//template <typename TFile, typename TRecords>
-//inline void readRecords(TRecords & records, PrefetchedFile<TFile, TRecords, Parallel> & me)
-//{
-//    readRecords(me.records, me.file, me.maxRecords);
-//    std::swap(records, me.records);
-//}
-
-// ----------------------------------------------------------------------------
-// Function getRecords<Serial>()
-// ----------------------------------------------------------------------------
-
-template <typename TFile, typename TRecords, typename TThreading>
-inline TRecords * getRecords(PrefetchedFile<TFile, TRecords, TThreading> & me)
-{
-    readRecords(me.records, me.file, me.maxRecords);
-
-    return &me.records;
-}
-
-// ----------------------------------------------------------------------------
-// Function getRecords<Parallel>()
-// ----------------------------------------------------------------------------
-
 template <typename TFile, typename TRecords>
-inline TRecords * getRecords(PrefetchedFile<TFile, TRecords, Parallel> & me)
+inline void readRecords(TRecords & records, PrefetchedFile<TFile, TRecords, Parallel> & me)
 {
-    // Wait the next batch of records.
+    // Wait the current batch of records.
     waitFor(me.reader);
 
-    // Make the next batch of records.
-    std::swap(me.records, me.reader.worker.records);
+    // Return the current batch of records.
+    swap(records, me.reader.worker.records);
 
     // Read the next batch of records.
     run(me.reader);
-
-    return me.records;
 }
 
 // ----------------------------------------------------------------------------
