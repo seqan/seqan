4ab34d65af7112c06ef4049c1adfc8cf0414b0ff
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2014-10-11 15:46:21 +0200

    [FIX] Fixing various issues.
    * initialization
    * bracket position

--- core/include/seqan/index/index_fm_stree.h
+++ core/include/seqan/index/index_fm_stree.h
@@ -57,7 +57,7 @@ struct HistoryStackFM_
     TAlphabet   lastChar;
 
     SEQAN_HOST_DEVICE
-    HistoryStackFM_() {}
+    HistoryStackFM_() : repLen(0), lastChar() {}
 
     template <typename TSize_, typename TAlphabet_>
     SEQAN_HOST_DEVICE
