7ffb4bb398fdd0a9ddf1e4d76835381a23b431c6
Hannes Hauswedell <h2+github.com@fsfe.org>
2014-10-14 11:15:21 +0200

    [fix] make modStrings open and saveable (NOOPs), make a stringset over a modString be assignable a stringset over its host's type

--- core/include/seqan/sequence/string_set_concat_direct.h
+++ core/include/seqan/sequence/string_set_concat_direct.h
@@ -107,6 +107,13 @@ public:
     }
 
     template <typename TOtherString, typename TOtherSpec>
+    StringSet(StringSet<TOtherString, TOtherSpec> &other)
+    {
+        _initStringSetLimits(*this);
+        assign(*this, other);
+    }
+
+    template <typename TOtherString, typename TOtherSpec>
     StringSet(StringSet<TOtherString, TOtherSpec> const &other)
     {
         _initStringSetLimits(*this);
@@ -284,7 +291,9 @@ template <typename TString, typename TSpec, typename TString2, typename TSpec2>
 void assign(StringSet<TString, Owner<ConcatDirect<TSpec> > > & stringSet,
             StringSet<TString2, Owner<ConcatDirect<TSpec2> > > & other)
 {
-    assign(stringSet, reinterpret_cast<StringSet<TString2, Owner<ConcatDirect<TSpec2> > > const &> (other));
+//     assign(stringSet, reinterpret_cast<StringSet<TString2, Owner<ConcatDirect<TSpec2> > > const &> (other));
+    assign(concat(stringSet), concat(other));
+    assign(stringSetLimits(stringSet), stringSetLimits(other));
 }
 
 // --------------------------------------------------------------------------
