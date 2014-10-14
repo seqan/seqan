27b6218e97d2887141ae42be7a3a147082327337
David Weese <dave.weese@googlemail.com>
2014-10-14 09:15:17 +0200

    [FIX] ported gff tutorials

--- extras/demos/tutorial/gff_io/solution3.cpp
+++ extras/demos/tutorial/gff_io/solution3.cpp
@@ -5,15 +5,12 @@
 
 int main()
 {
-    seqan::GffStream out("-", seqan::GffStream::WRITE);
-
-    // Add sequence names.
-    addSequenceName(out, "ctg123");
+    seqan::GffFileOut out(std::cout, seqan::Gff());
 
     // Write out the records.
     seqan::GffRecord record;
 
-    record.rID = 0;
+    record.ref = "ctg123";
     record.source = "";
     record.type = "gene";
     record.beginPos = 999;
@@ -24,13 +21,12 @@ int main()
     appendValue(record.tagValues, "gene0001");
     appendValue(record.tagNames, "Name");
     appendValue(record.tagValues, "EDEN");
-    if (writeRecord(out, record) != 0)
-        std::cerr << "ERROR: Problem writing output file.";
+    writeRecord(out, record);
 
     clear(record.tagNames);
     clear(record.tagValues);
 
-    record.rID = 0;
+    record.ref = "ctg123";
     record.source = "";
     record.type = "TF_binding_site";
     record.beginPos = 999;
@@ -39,8 +35,7 @@ int main()
     record.score = seqan::GffRecord::INVALID_SCORE();
     appendValue(record.tagNames, "Parent");
     appendValue(record.tagValues, "gene0001");
-    if (writeRecord(out, record) != 0)
-        std::cerr << "ERROR: Problem writing output file.";
+    writeRecord(out, record);
     
     return 0;
 }
