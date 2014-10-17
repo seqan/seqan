bd22ad9e84885b047739647155c3b9b335f68785
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2014-10-17 10:50:15 +0200

    [FEATURE] Adding const access for VcfIOContext.

--- extras/include/seqan/vcf_io/vcf_io_context.h
+++ extras/include/seqan/vcf_io/vcf_io_context.h
@@ -190,8 +190,22 @@ contigNames(VcfIOContext<TNameStore, TNameStoreCache, TStorageSpec> & context)
 }
 
 template <typename TNameStore, typename TNameStoreCache, typename TStorageSpec>
+inline TNameStore const &
+contigNames(VcfIOContext<TNameStore, TNameStoreCache, TStorageSpec> const & context)
+{
+    return _referenceCast<TNameStore &>(context._contigNames);
+}
+
+template <typename TNameStore, typename TNameStoreCache, typename TStorageSpec>
 inline TNameStoreCache &
 contigNamesCache(VcfIOContext<TNameStore, TNameStoreCache, TStorageSpec> & context)
+{
+    return _referenceCast<TNameStoreCache &>(context._contigNamesCache);
+}
+
+template <typename TNameStore, typename TNameStoreCache, typename TStorageSpec>
+inline TNameStoreCache const &
+contigNamesCache(VcfIOContext<TNameStore, TNameStoreCache, TStorageSpec> const & context)
 {
     return _referenceCast<TNameStoreCache &>(context._contigNamesCache);
 }
@@ -204,8 +218,22 @@ sampleNames(VcfIOContext<TNameStore, TNameStoreCache, TStorageSpec> & context)
 }
 
 template <typename TNameStore, typename TNameStoreCache, typename TStorageSpec>
+inline TNameStore const &
+sampleNames(VcfIOContext<TNameStore, TNameStoreCache, TStorageSpec> const & context)
+{
+    return _referenceCast<TNameStore &>(context._sampleNames);
+}
+
+template <typename TNameStore, typename TNameStoreCache, typename TStorageSpec>
 inline TNameStoreCache &
 sampleNamesCache(VcfIOContext<TNameStore, TNameStoreCache, TStorageSpec> & context)
+{
+    return _referenceCast<TNameStoreCache &>(context._sampleNamesCache);
+}
+
+template <typename TNameStore, typename TNameStoreCache, typename TStorageSpec>
+inline TNameStoreCache const &
+sampleNamesCache(VcfIOContext<TNameStore, TNameStoreCache, TStorageSpec> const & context)
 {
     return _referenceCast<TNameStoreCache &>(context._sampleNamesCache);
 }
