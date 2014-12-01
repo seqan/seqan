bf6fd0f48fc065697e06cc0ed8232d07803d6d8f
Hannes Hauswedell <h2+github.com@fsfe.org>
2014-12-01 15:03:53 +0100

    [INTERNAL] mv basic file from extras to core

--- core/include/seqan/basic.h
+++ core/include/seqan/basic.h
@@ -106,6 +106,7 @@
 // Basic device metafunctions.
 #include <seqan/basic/basic_device.h>
 
-
+// Basic SIMD vector
+#include <seqan/basic/basic_simd_vector.h>
 
 #endif  // #ifndef SEQAN_CORE_INCLUDE_SEQAN_BASIC_H_
