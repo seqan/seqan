e70e732c3ceeb069ceb623516a220d11d3f4b4cb
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-12-02 18:10:46 +0100

    [DOC] Manual: Bam I/O

--- demos/tutorial/bam_io/solution3.cpp
+++ demos/tutorial/bam_io/solution3.cpp
@@ -1,61 +1,42 @@
-#include <iostream>
-#include <seqan/sequence.h>
 #include <seqan/bam_io.h>
 
 int main()
 {
-    seqan::Dna5String ref = "CCCGATGAGCACACGATCACACGATGACA";
-
-    // --------------------------------------------------------
-    // Build header.
-    // --------------------------------------------------------
-    seqan::BamFileOut bamFileOut(std::cout, seqan::Sam());
-
-    // Fill sequenceInfos.
-    assignValueById(sequenceLengths(context(bamFileOut)),
-                    nameToId(nameStoreCache(context(bamFileOut)), "REF"),
-                    length(ref));
-
-    // Fill header records.
-    seqan::BamHeader header;
-    resize(header, 1);
-    // @HD header.
-    header[0].type = seqan::BAM_HEADER_FIRST;
-    resize(header[0].tags, 1);
-    // @HD header, tag/value: VN:1.4.
-    header[0].tags[0].i1 = "VN";
-    header[0].tags[0].i2 = "1.4";
-
-    writeRecord(bamFileOut, header);
-
-    // --------------------------------------------------------
-    // Write out records.
-    // --------------------------------------------------------
-
-    seqan::BamAlignmentRecord record;
-
-    for (unsigned i = 0; i + 12 - 1 < length(ref); ++i)
+    // Open input file.
+    seqan::BamFileIn bamFileIn;
+    if (!open(bamFileIn, "example.sam"))
     {
-        clear(record);
-        // Set members that are the same for all records.
-        record.rID = 0;
-        record.flag = 0;
-        resize(record.cigar, 1);
-        record.cigar[0].operation = '=';
-        record.cigar[0].count = 12;
-
-        // The query name is REF_${START}_${END}.
-        record.qName = "REF_";
-        appendNumber(record.qName, i);
-        appendValue(record.qName, '_');
-        appendNumber(record.qName, i + 12);
-        // Set position.
-        record.beginPos = i;
-        // Set sequence.
-        record.seq = infix(ref, i, i + 12);
-
-        writeRecord(bamFileOut, record);
+        std::cerr << "ERROR: Could not open example.sam!" << std::endl;
+        return 1;
     }
+
+    try
+    {
+        // Read header.
+        seqan::BamHeader header;
+        readRecord(header, bamFileIn);
+
+        unsigned tagIdx = 0;
+        unsigned numXXtags = 0;
+
+        // Rear records.
+        seqan::BamAlignmentRecord record;
+        while (!atEnd(bamFileIn))
+        {
+            readRecord(record, bamFileIn);
+            seqan::BamTagsDict tagsDict(record.tags);
+
+            if (findTagKey(tagIdx, tagsDict, "XX"))
+                numXXtags += 1;
+        }
+    }
+    catch (seqan::IOError const & e)
+    {
+        std::cout << "ERROR: " << e.what() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Number of records with the XX tag: " << numXXtags << "\n";
 
     return 0;
 }
