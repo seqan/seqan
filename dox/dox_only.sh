9246d5d9134a70ed1c6ab4bd6e1ff63ebc826b42
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2014-12-03 19:15:27 +0100

    [FIX] Fixing dox_only.sh script for building dox

--- dox/dox_only.sh
+++ dox/dox_only.sh
@@ -2,4 +2,4 @@
 
 # Run doxygen-style documentation system.
 
-rm -f dddoc_cache.bin && ../util/bin/dox.py -b .. -i ../include/seqan -i pages --image-dir ../images/docs2 --image-dir ./images/reduced_aminoacid/ $*
+rm -f dddoc_cache.bin && ../util/bin/dox.py -b .. -i ../include/seqan -i pages --image-dir ./images/docs2 --image-dir ./images/reduced_aminoacid/ $*
