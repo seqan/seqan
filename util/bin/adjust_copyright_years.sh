4dc221d5ad71e73b1521c37105af4479eec1a10a
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2015-01-19 21:45:02 +0100

    [INTERNAL] Util: updated adjust_copyright_years.sh

--- util/bin/adjust_copyright_years.sh
+++ util/bin/adjust_copyright_years.sh
@@ -6,11 +6,11 @@
 # Note: You have to replace the until date in this document.
 # Note: Call this file from the seqan root directory.
 
-for i in $( find {include,tests,demos} {include,tests,demos} -name "*.h" -o -name "*.cpp" ); do
-    sed -i '' 's/Copyright (c) 20..-20.., Knut Reinert, FU Berlin/Copyright (c) 2006-2013, Knut Reinert, FU Berlin/g' $i
+for i in $( find {include,tests,demos,apps} -name "*.h" -o -name "*.cpp" ); do
+    sed -i '' 's/Copyright (c) 20..-20.., Knut Reinert, FU Berlin/Copyright (c) 2006-2015, Knut Reinert, FU Berlin/g' $i
 done
 
-for i in $( find {include,tests} {include,tests} -name "INFO" ); do
-    sed -i '' 's/Copyright: 20..-20.*, FU Berlin/Copyright: 2006-2013, FU Berlin/g' $i
+for i in $( find {include,tests,apps} -name "INFO" ); do
+    sed -i '' 's/Copyright: 20..-20.*, FU Berlin/Copyright: 2006-2015, FU Berlin/g' $i
 done
 
