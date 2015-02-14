736de976ef00689fb54721d9c9e957dfc777269f
Sascha Meiers <meiers@embl.de>
2015-02-14 18:29:54 +0100

    [FIX] type of valuesize for finite alphabets needs to accomodate for alphabet sizes > 255

--- include/seqan/basic/alphabet_residue.h
+++ include/seqan/basic/alphabet_residue.h
@@ -592,8 +592,7 @@ struct Finite;
 template <typename TValue, unsigned SIZE>
 struct ValueSize<SimpleType<TValue, Finite<SIZE> > >
 {
-    //TODO(meiers): this needs to be change to hold SIZE many values
-    typedef __uint8 Type;
+    typedef __uint64 Type;
     static const Type VALUE = SIZE;
 };
 
