c3c350cf406cf3e21bfca737c082a2c8dd64fa9d
Hannes Hauswedell <h2+github.com@fsfe.org>
2015-02-17 18:34:28 +0100

    split files

--- tests/blast/test_blast_output.h
+++ tests/blast/test_blast_output.h
@@ -1,7 +1,7 @@
 // ==========================================================================
 //                 SeqAn - The Library for Sequence Analysis
 // ==========================================================================
-// Copyright (c) 2014, Knut Reinert, FU Berlin
+// Copyright (c) 2006-2015, Knut Reinert, FU Berlin
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -29,13 +29,13 @@
 // DAMAGE.
 //
 // ==========================================================================
-// Author: Hannes Hauswedell <hauswedell@mi.fu-berlin.de>
+// Author: Hannes Hauswedell <hannes.hauswedell@fu-berlin.de>
 // ==========================================================================
-// Tests for basic/translation.h
+// Tests for the blast module
 // ==========================================================================
 
-#ifndef SEQAN_EXTRAS_TESTS_BASIC_TEST_BLAST_H_
-#define SEQAN_EXTRAS_TESTS_BASIC_TEST_BLAST_H_
+#ifndef SEQAN_TESTS_TEST_BLAST_OUTPUT_H_
+#define SEQAN_TESTS_TEST_BLAST_OUTPUT_H_
 
 #include <seqan/basic.h>
 #include <seqan/sequence.h>
@@ -44,249 +44,6 @@
 
 using namespace seqan;
 
-// ------------------------- STATISTICS ---------------------------------------
-
-SEQAN_DEFINE_TEST(test_blast_scoring_scheme_conversion)
-{
-    Blosum62 scheme;
-    setScoreGapOpen(scheme, -11);
-    setScoreGapExtend(scheme, -1);
-
-    blastScoringScheme2seqanScoringScheme(scheme);
-
-    SEQAN_ASSERT_EQ(scoreGapOpen(scheme), -12);
-    SEQAN_ASSERT_EQ(scoreGapExtend(scheme), -1);
-
-    seqanScoringScheme2blastScoringScheme(scheme);
-
-    SEQAN_ASSERT_EQ(scoreGapOpen(scheme), -11);
-    SEQAN_ASSERT_EQ(scoreGapExtend(scheme), -1);
-}
-
-SEQAN_DEFINE_TEST(test_blast_scoring_adapter)
-{
-    // Blosum62 and some general stuff
-    {
-        typedef Blosum62 TScheme;
-        TScheme scheme;
-        setScoreGapOpen(scheme, -11);
-        setScoreGapExtend(scheme, -1);
-        blastScoringScheme2seqanScoringScheme(scheme);
-
-        // TEST CONSTRUCTOR
-        BlastScoringAdapter<TScheme> adapter(scheme);
-        // TEST isValid()
-        SEQAN_ASSERT(isValid(adapter));
-        // TEST get*
-        SEQAN_ASSERT_EQ(getLambda(adapter), 0.267);
-        SEQAN_ASSERT_EQ(getKappa(adapter),  0.041);
-        SEQAN_ASSERT_EQ(getH(adapter),      0.140);
-        SEQAN_ASSERT_EQ(getAlpha(adapter),  1.900);
-        SEQAN_ASSERT_EQ(getBeta(adapter), -30.000);
-
-        setScoreGapOpen(scheme, -1);
-        setScoreGapExtend(scheme, -11); // invalid parameters
-        // TEST assignScoreScheme
-        bool res = assignScoreScheme(adapter, scheme);
-        SEQAN_ASSERT(!res);
-
-        // TEST getScoreScheme
-        Blosum62 const & schemeInt = getScoreScheme(adapter);
-        SEQAN_ASSERT_EQ(scoreGapOpen(schemeInt), -1);
-        SEQAN_ASSERT_EQ(scoreGapExtend(schemeInt), -11);
-
-        // reset to valid
-        setScoreGapOpen(scheme, -11);
-        setScoreGapExtend(scheme, -1);
-        blastScoringScheme2seqanScoringScheme(scheme);
-        assignScoreScheme(adapter, scheme);
-        // now check values again
-        SEQAN_ASSERT(isValid(adapter));
-        // TEST get*
-        SEQAN_ASSERT_EQ(getLambda(adapter), 0.267);
-        SEQAN_ASSERT_EQ(getKappa(adapter),  0.041);
-        SEQAN_ASSERT_EQ(getH(adapter),      0.140);
-        SEQAN_ASSERT_EQ(getAlpha(adapter),  1.900);
-        SEQAN_ASSERT_EQ(getBeta(adapter), -30.000);
-    }
-
-    // Blosum45
-    {
-        typedef Blosum45 TScheme;
-        TScheme scheme;
-        setScoreGapOpen(scheme, -11);
-        setScoreGapExtend(scheme, -3);
-        blastScoringScheme2seqanScoringScheme(scheme);
-
-        // TEST CONSTRUCTOR
-        BlastScoringAdapter<TScheme> adapter(scheme);
-        // TEST isValid()
-        SEQAN_ASSERT(isValid(adapter));
-        // TEST get*
-        SEQAN_ASSERT_EQ(getLambda(adapter), 0.190);
-        SEQAN_ASSERT_EQ(getKappa(adapter),  0.031);
-        SEQAN_ASSERT_EQ(getH(adapter),      0.095);
-        SEQAN_ASSERT_EQ(getAlpha(adapter),  2.000);
-        SEQAN_ASSERT_EQ(getBeta(adapter), -38.000);
-    }
-
-    // Blosum80
-    {
-        typedef Blosum80 TScheme;
-        TScheme scheme;
-        setScoreGapOpen(scheme, -11);
-        setScoreGapExtend(scheme, -1);
-        blastScoringScheme2seqanScoringScheme(scheme);
-
-        // TEST CONSTRUCTOR
-        BlastScoringAdapter<TScheme> adapter(scheme);
-        // TEST isValid()
-        SEQAN_ASSERT(isValid(adapter));
-        // TEST get*
-        SEQAN_ASSERT_EQ(getLambda(adapter), 0.314);
-        SEQAN_ASSERT_EQ(getKappa(adapter),  0.095);
-        SEQAN_ASSERT_EQ(getH(adapter),      0.350);
-        SEQAN_ASSERT_EQ(getAlpha(adapter),  0.900);
-        SEQAN_ASSERT_EQ(getBeta(adapter),  -9.000);
-    }
-
-    // Pam250
-    {
-        typedef Pam250 TScheme;
-        TScheme scheme;
-        setScoreGapOpen(scheme, -11);
-        setScoreGapExtend(scheme, -3);
-        blastScoringScheme2seqanScoringScheme(scheme);
-
-        // TEST CONSTRUCTOR
-        BlastScoringAdapter<TScheme> adapter(scheme);
-        // TEST isValid()
-        SEQAN_ASSERT(isValid(adapter));
-        // TEST get*
-        SEQAN_ASSERT_EQ(getLambda(adapter), 0.174);
-        SEQAN_ASSERT_EQ(getKappa(adapter),  0.020);
-        SEQAN_ASSERT_EQ(getH(adapter),      0.070);
-        SEQAN_ASSERT_EQ(getAlpha(adapter),  2.500);
-        SEQAN_ASSERT_EQ(getBeta(adapter), -48.000);
-    }
-
-    // SimpleScore
-    {
-        typedef Score<int, Simple> TScheme;
-        TScheme scheme;
-        setScoreMatch(scheme, 2);
-        setScoreMismatch(scheme, -3);
-        setScoreGapOpen(scheme, -4);
-        setScoreGapExtend(scheme, -2);
-        blastScoringScheme2seqanScoringScheme(scheme);
-
-        // TEST CONSTRUCTOR
-        BlastScoringAdapter<TScheme> adapter(scheme);
-        // TEST isValid()
-        SEQAN_ASSERT(isValid(adapter));
-        // TEST get*
-        SEQAN_ASSERT_EQ(getLambda(adapter), 0.610);
-        SEQAN_ASSERT_EQ(getKappa(adapter),  0.350);
-        SEQAN_ASSERT_EQ(getH(adapter),      0.680);
-        SEQAN_ASSERT_EQ(getAlpha(adapter),  0.900);
-        SEQAN_ASSERT_EQ(getBeta(adapter),  -3.000);
-    }
-}
-
-SEQAN_DEFINE_TEST(test_blast_blastmatch_stats_and_score)
-{
-    typedef Align<String<AminoAcid>, ArrayGaps> TAlign;
-    typedef BlastMatch<CharString, CharString, uint32_t, TAlign> TBlastMatch;
-
-    TBlastMatch m;
-
-    String<AminoAcid> src0 = "ARNDAYVBRNDCQFGCYVBQARNDCQEGEG";
-    String<AminoAcid> src1 = "ARNAYVBRNDCCYCYVBQARNQEGEG";
-
-    resize(rows(m.align), 2);
-    assignSource(row(m.align, 0), src0);
-    assignSource(row(m.align, 1), src1);
-
-    typedef Blosum62 TScheme;
-    TScheme scheme;
-    setScoreGapOpen(scheme, -11);
-    setScoreGapExtend(scheme, -1);
-    blastScoringScheme2seqanScoringScheme(scheme);
-
-    int score = globalAlignment(m.align, scheme);
-//     ARNDAYVBRNDCQFGCYVBQARNDCQEGEG
-//     ||| ||||||||   ||||||||  |||||
-//     ARN-AYVBRNDCCY-CYVBQARN--QEGEG
-
-    SEQAN_ASSERT_EQ(score, 94);
-
-    calcStatsAndScore(m, scheme);
-
-    SEQAN_ASSERT_EQ(m.score, score);
-    SEQAN_ASSERT_EQ(m.aliLength, 30u);
-    SEQAN_ASSERT_EQ(m.identities, 24u);
-    SEQAN_ASSERT_EQ(m.positives, 25u);
-    SEQAN_ASSERT_EQ(m.mismatches, 2u);
-    SEQAN_ASSERT_EQ(m.gaps, 4u);
-    SEQAN_ASSERT_EQ(m.gapOpenings, 3u);
-}
-
-SEQAN_DEFINE_TEST(test_blast_blastmatch_bit_score_e_value)
-{
-    typedef Align<String<AminoAcid>, ArrayGaps> TAlign;
-    typedef BlastMatch<CharString, CharString, uint32_t, TAlign> TBlastMatch;
-    typedef Blosum62 TScheme;
-
-    TBlastMatch m;
-
-    String<AminoAcid> src0 =
-    "SSITEEKHIPHKEQDKDAEFLSKEALKTHMTENVLQMDRRAVQDPSTSFLQLLKAKGLLG"
-    "LPDYEVNLADVNSPGFRKVAYAQTKPRRLCFPNGGTRRGSFIMDTAVVVMVSLRYVNIGK"
-    "VIFPGATDVSEGEDEFWAGLPQAYGCLATEFLCIHIAIYSWIHVQSSRYDDMNASVIRAK"
-    "LNLPGGLTLIQARGNEKETI";
-    String<AminoAcid> src1 = "VAYAQPRKLCYP";
-
-    resize(rows(m.align), 2);
-    assignSource(row(m.align, 0), src0);
-    assignSource(row(m.align, 1), src1);
-
-    TScheme scheme;
-    setScoreGapOpen(scheme, -11);
-    setScoreGapExtend(scheme, -1);
-    blastScoringScheme2seqanScoringScheme(scheme);
-
-    int score = localAlignment(m.align, scheme);
-//         VAYAQTKPRRLCFP
-//         |||||  || || |
-//         VAYAQ--PRKLCYP
-
-    SEQAN_ASSERT_EQ(score, 48);
-
-    calcStatsAndScore(m, scheme);
-
-    SEQAN_ASSERT_EQ(m.score, score);
-    SEQAN_ASSERT_EQ(m.aliLength, 14u);
-    SEQAN_ASSERT_EQ(m.identities, 10u);
-    SEQAN_ASSERT_EQ(m.positives, 12u);
-    SEQAN_ASSERT_EQ(m.mismatches, 2u);
-    SEQAN_ASSERT_EQ(m.gaps, 2u);
-    SEQAN_ASSERT_EQ(m.gapOpenings, 1u);
-
-    // same as previous test until here (just local)
-
-    BlastScoringAdapter<TScheme> adapter(scheme);
-    SEQAN_ASSERT(isValid(adapter));
-
-    calcBitScoreAndEValue(m.bitScore, m.eValue, m.score, length(src0),
-                          length(src1), adapter);
-
-    double epsilon = 1e-4;
-    SEQAN_ASSERT_LEQ(std::abs(m.bitScore - 23.0978), epsilon);
-    epsilon = 1e-8; // more important on evalue
-    SEQAN_ASSERT_LEQ(std::abs(m.eValue - 0.000267348), epsilon);
-}
-
-// ------------------------- OUTPUT ---------------------------------------
 
 template <typename TFile, typename TDbSpecs, typename TRecords, typename TFormat>
 inline void
@@ -590,7 +347,6 @@ void test_blast_write_tabular_impl(BlastFormat<f, p, g> const & /**/,
         "Query_Numero_Uno\tSubject_Numero_Uno\t14\t2\t2\t0.000534696\t23.0978\n"
         "Query_Numero_Uno\tSubject_Numero_Dos\t8\t0\t0\t0.000912053\t22.3274\n");
 
-
     if (f == BlastFormatFile::TABULAR_WITH_HEADER)
     {
         std::string header = "# BLASTP I/O Module of SeqAn-";
@@ -618,8 +374,8 @@ void test_blast_write_tabular_impl(BlastFormat<f, p, g> const & /**/,
         if (g == BlastFormatGeneration::BLAST_PLUS)
             header.append("# 2 hits found\n");
         compString.append(header);
-
     }
+
     if (customFields == 0)
         compString.append(
         "Query_Numero_Dos\tSubject_Numero_Uno\t87.50\t8\t1\t0\t1\t8\t184\t191\t0.026\t16.9\n"
@@ -632,7 +388,6 @@ void test_blast_write_tabular_impl(BlastFormat<f, p, g> const & /**/,
         compString.append(
         "Query_Numero_Dos\tSubject_Numero_Uno\t8\t1\t0\t0.0255459\t16.9346\n"
         "Query_Numero_Dos\tSubject_Numero_Dos\t8\t0\t0\t0.00672262\t18.8606\n");
-
 
     if (contents != compString)
     {
@@ -753,7 +508,11 @@ SEQAN_DEFINE_TEST(test_blast_write_pairwise)
     char filenameBuffer[1000];
     strcpy(filenameBuffer, tempFilename);
 
-    std::fstream fstream(filenameBuffer, std::ios_base::in | std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
+    std::fstream fstream(filenameBuffer,
+                         std::ios_base::in |
+                         std::ios_base::out |
+                         std::ios_base::binary |
+                         std::ios_base::trunc);
     SEQAN_ASSERT(fstream.is_open());
 
     test_blast_write_record_match(fstream, false, TFormat());
@@ -899,4 +658,4 @@ SEQAN_DEFINE_TEST(test_blast_write_pairwise)
 //     std::cout << "</table></span>\n";
 }
 
-#endif  // SEQAN_EXTRAS_TESTS_BASIC_TEST_BLAST_H_
+#endif  // SEQAN_TESTS_TEST_BLAST_OUTPUT_H_
