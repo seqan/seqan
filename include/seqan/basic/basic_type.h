be3c3606f41fd7de5d50881507169870e2b2d9d6
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2015-05-04 19:01:51 +0200

    [FIX] Basic: fixed const _toParamenter()

--- include/seqan/basic/basic_type.h
+++ include/seqan/basic/basic_type.h
@@ -359,7 +359,7 @@ SEQAN_HOST_DEVICE inline _toParameter(T & _object)
 }
 
 template <typename T>
-typename Parameter_<T const>::Type
+typename Parameter_<T>::Type
 SEQAN_HOST_DEVICE inline _toParameter(T const & _object)
 {
     return _object;
