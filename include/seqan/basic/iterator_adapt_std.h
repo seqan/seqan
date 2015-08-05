5f442482f0571e412116f8df51554cbb9767e39d
benjamin menkuec <benjamin@menkuec.de>
2015-08-05 22:46:25 +0200

    [FIX] compile fix for GCC and CLANG

--- include/seqan/basic/iterator_adapt_std.h
+++ include/seqan/basic/iterator_adapt_std.h
@@ -57,12 +57,15 @@ namespace std
     };
 
 	// there is a bug in vc2015 stl, it doesnt check the iterator_traits correctly
-	// I have reported this bug to microsoft already. For now, this is a workaround.
+	// I have reported this bug to microsoft already (filed as DevDiv#1208117). 
+	// For now, this is a workaround.
+#if _MSC_VER == 1900
 	template<class _Ty, class Tag>
 	struct _Is_iterator<typename seqan::Iter<_Ty, Tag>>
 		: true_type
 	{
 	};
+#endif
 }
 
 namespace seqan {
