c9b9353d09764703db4d5d47e7d0fc4098b9a8ac
benjamin menkuec <benjamin@menkuec.de>
2015-09-27 13:20:19 +0200

    ReadThread speed improvement

--- apps/seqan_flexbar/seqan_flexlib.cpp
+++ apps/seqan_flexbar/seqan_flexlib.cpp
@@ -1553,6 +1553,7 @@ struct ReadReader
                         readSet.first.unlock();
                     }
                 }
+                std::this_thread::sleep_for(std::chrono::milliseconds(50));
             }
         });
     };
