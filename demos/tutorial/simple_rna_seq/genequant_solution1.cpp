56d7e3e2fe1ada7ee829f4f0450415029b18c86c
Temesgen H. Dadi <temehi@gmail.com>
2015-10-01 22:40:16 +0200

    [DOC] tutorial fixes simple_rna_seq

--- demos/tutorial/simple_rna_seq/genequant_solution1.cpp
+++ demos/tutorial/simple_rna_seq/genequant_solution1.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <seqan/store.h>
-#include <seqan/arg_parse.h>
 #include <seqan/misc/interval_tree.h>
 #include <seqan/parallel.h>
 
@@ -10,68 +9,32 @@ using namespace seqan;
 // define used types
 typedef FragmentStore<> TStore;
 
-
-// define options
-struct Options
-{
-    std::string annotationFileName;
-    std::string alignmentFileName;
-};
-
-
-//
-// 1. Parse command line and fill Options object
-//
-ArgumentParser::ParseResult parseOptions(Options & options, int argc, char const * argv[])
-{
-    ArgumentParser parser("gene_quant");
-    setShortDescription(parser, "A simple gene quantification tool");
-    setVersion(parser, "1.0");
-    setDate(parser, "Sep 2012");
-
-    addArgument(parser, ArgParseArgument(ArgParseArgument::INPUT_FILE));
-    addArgument(parser, ArgParseArgument(ArgParseArgument::INPUT_FILE));
-    addUsageLine(parser, "[\\fIOPTIONS\\fP] <\\fIANNOTATION FILE\\fP> <\\fIREAD ALIGNMENT FILE\\fP>");
-
-    // Parse command line
-    ArgumentParser::ParseResult res = parse(parser, argc, argv);
-
-    if (res == ArgumentParser::PARSE_OK)
-    {
-        // Extract option values
-        getArgumentValue(options.annotationFileName, parser, 0);
-        getArgumentValue(options.alignmentFileName, parser, 1);
-    }
-
-    return res;
-}
-
 //![solution]
 //
-// 2. Load annotations and alignments from files
+// 1. Load annotations and alignments from files
 //
-bool loadFiles(TStore & store, Options const & options)
+bool loadFiles(TStore & store, std::string const & annotationFileName,  std::string const & alignmentFileName)
 {
     BamFileIn alignmentFile;
-    if (!open(alignmentFile, options.alignmentFileName.c_str()))
+    if (!open(alignmentFile, alignmentFileName.c_str()))
     {
-        std::cerr << "Couldn't open alignment file " << options.alignmentFileName << std::endl;
+        std::cerr << "Couldn't open alignment file " << alignmentFileName << std::endl;
         return false;
     }
-    std::cerr << "Loading read alignments ..... " << std::flush;
+    std::cout << "Loading read alignments ..... " << std::flush;
     readRecords(store, alignmentFile);
-    std::cerr << "[" << length(store.alignedReadStore) << "]" << std::endl;
+    std::cout << "[" << length(store.alignedReadStore) << "]" << std::endl;
 
     // load annotations
     GffFileIn annotationFile;
-    if (!open(annotationFile, options.annotationFileName.c_str()))
+    if (!open(annotationFile, toCString(annotationFileName)))
     {
-        std::cerr << "Couldn't open annotation file" << options.annotationFileName << std::endl;
+        std::cerr << "Couldn't open annotation file" << annotationFileName << std::endl;
         return false;
     }
-    std::cerr << "Loading genome annotation ... " << std::flush;
+    std::cout << "Loading genome annotation ... " << std::flush;
     readRecords(store, annotationFile);
-    std::cerr << "[" << length(store.annotationStore) << "]" << std::endl;
+    std::cout << "[" << length(store.annotationStore) << "]" << std::endl;
 
     return true;
 }
@@ -80,14 +43,11 @@ bool loadFiles(TStore & store, Options const & options)
 //![main]
 int main(int argc, char const * argv[])
 {
-    Options options;
     TStore store;
+    std::string annotationFileName = getAbsolutePath("/demos/tutorial/simple_rna_seq/example.gtf");
+    std::string alignmentFileName = getAbsolutePath("/demos/tutorial/simple_rna_seq/example.sam");
 
-    ArgumentParser::ParseResult res = parseOptions(options, argc, argv);
-    if (res != ArgumentParser::PARSE_OK)
-        return res == ArgumentParser::PARSE_ERROR;
-
-    if (!loadFiles(store, options))
+    if (!loadFiles(store, annotationFileName, alignmentFileName))
         return 1;
 
     return 0;
