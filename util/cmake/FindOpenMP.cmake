19cb4a43f4cf5340f8dd69ac8b7d3937031ed389
Hannes Hauswedell <h2+github.com@fsfe.org>
2015-10-26 18:01:53 +0100

    [FIX] undo most of #1219, add diagnostic output

--- util/cmake/FindOpenMP.cmake
+++ util/cmake/FindOpenMP.cmake
@@ -38,6 +38,7 @@ endif ()
 # Clang-3.7.0 deactivated because of compiler bugs triggered by SeqAn
 if (COMPILER_IS_CLANG AND (_GCC_VERSION EQUAL 370))
     set(_OPENMP_NOT_FOUND TRUE)
+    message (STATUS "OpenMP cannot be used becase Clang-3.7.0 has a bug. Please update your clang!")
     return ()
 endif ()
 
@@ -46,9 +47,7 @@ include(CheckCXXSourceCompiles)
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 
 set(OpenMP_C_FLAG_CANDIDATES
-  #Clang
-  "-fopenmp=libomp"
-  #Gnu
+  #Gnu & LLVM
   "-fopenmp"
   #Microsoft Visual Studio
   "/openmp"
