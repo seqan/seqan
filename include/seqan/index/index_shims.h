b13d978784d5b1618fb9722bee238e6b24704cb9
Hannes Hauswedell <h2+github.com@fsfe.org>
2015-12-02 16:14:48 +0100

    [FIXED-#507] small fix for big problem?

--- include/seqan/index/index_shims.h
+++ include/seqan/index/index_shims.h
@@ -840,10 +840,10 @@ inline bool indexCreate(Index<TText, TSpec> & index, FibreSA, Trie)
     template <typename TValue>
     inline bool open(TValue & value, const char *fileName, int openMode)
     {
-        String<TValue, External< ExternalConfigLarge<> > > extString;
-        if (!open(extString, fileName, openMode & ~OPEN_CREATE)) return false;
-        if (!empty(extString)) assign(value, front(extString));
-        return true;
+        File<> f;
+        if (!open(f, fileName, openMode & ~OPEN_CREATE))
+            return false;
+        return read(f, &value, 1);
     }
 
     template <typename TValue>
@@ -965,11 +965,10 @@ inline bool indexCreate(Index<TText, TSpec> & index, FibreSA, Trie)
     template <typename TValue>
     inline bool save(TValue const &val, const char *fileName, int openMode)
     {
-        String<TValue, External< ExternalConfigLarge<> > > extString;
-        if (!open(extString, fileName, openMode)) return false;
-        clear(extString);
-        appendValue(extString, val);
-        return true;
+        File<> f;
+        if (!open(f, fileName, openMode))
+            return false;
+        return write(f, &value, 1);
     }
 
     template <typename TValue>
