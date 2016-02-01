dcd6dc034b40d73d000b20643241443a87a30f44
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2016-02-01 13:59:21 +0100

    [FIX] BasicTechniques.rst - removed all code-block elements.

---
+++ demos/tutorial/basic_techniques/base.cpp
@@ -0,0 +1,15 @@
+#include <seqan/basic.h>
+//![class]
+template <typename T>
+class vector{};
+//![class]
+
+using namespace seqan;
+
+int main(){
+//![vector]
+vector<int> my_vector;
+//![vector]
+	ignoreUnusedVariableWarning(my_vector);
+	return 0;
+}
