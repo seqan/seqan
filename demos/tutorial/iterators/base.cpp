2e67217ae46a577edefaf7d82f4d0bb9cbd47135
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2016-02-03 14:55:14 +0100

    [FIX] Iterators.rst - removed all code-block elements.

---
+++ demos/tutorial/iterators/base.cpp
@@ -0,0 +1,28 @@
+#include <seqan/sequence.h>
+#include <iostream>
+
+using namespace seqan;
+
+int main() {
+//![construction]
+    Iterator<DnaString>::Type           it1;  // A standard iterator
+    Iterator<DnaString, Standard>::Type it2;  // Same as above
+    Iterator<DnaString, Rooted>::Type   it3;  // A rooted iterator
+//![construction]
+    ignoreUnusedVariableWarning(it1);
+    ignoreUnusedVariableWarning(it2);
+    ignoreUnusedVariableWarning(it3);
+
+std::cout << "\n//![use-case]\n";
+//![use-case]
+    DnaString genome = "ACGTACGTACGT";
+    typedef Iterator<DnaString>::Type TIterator;
+    for (TIterator it = begin(genome); it != end(genome); goNext(it))
+    {
+        std::cout << value(it);
+    }
+//![use-case]
+std::cout << "\n//![use-case]\n";
+
+    return 0;
+}
