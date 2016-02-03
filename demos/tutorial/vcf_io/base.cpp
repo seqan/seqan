7f32856e4c56d66e3d5ef52055db8b768ed169b9
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2016-02-03 19:08:33 +0100

    [FIX] VcfIO.rst - removed all code-block elements.

---
+++ demos/tutorial/vcf_io/base.cpp
@@ -0,0 +1,29 @@
+#include <seqan/sequence.h>
+
+//![vcfRecord]
+using namespace seqan;
+
+class VcfRecord
+{
+public:
+   __int32 rID;                          // CHROM
+   __int32 beginPos;                     // POS
+   CharString id;                        // ID
+   CharString ref;                       // REF
+   CharString alt;                       // ALT
+   float qual;                           // QUAL
+   CharString filter;                    // FILTER
+   CharString info;                      // INFO
+   CharString format;                    // FORMAT
+   StringSet<CharString> genotypeInfos;  // <individual1> <individual2> ..
+
+   // Constants for marking reference id and position as invalid.
+   static const __int32 INVALID_REFID = -1;
+   static const __int32 INVALID_POS = -1;
+   // This function returns the float value for "invalid quality".
+   static float MISSING_QUAL();
+};
+//![vcfRecord]
+int main(){
+	return 0;
+}
