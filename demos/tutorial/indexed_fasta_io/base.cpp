1e5695ab5207f83b295baa2fb5d819eb2cdcea62
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2016-02-05 16:40:31 +0100

    [FIX] fixed second test (iterators_base). To avoid further issues all std::cerr were changed to std::cout.

--- demos/tutorial/indexed_fasta_io/base.cpp
+++ demos/tutorial/indexed_fasta_io/base.cpp
@@ -12,35 +12,35 @@ int main(){
 
 //![open_index1]
     if (!build(faiIndex, toCString(pathToFile)))
-        std::cerr << "ERROR: Could not build the index!\n";
+        std::cout << "ERROR: Could not build the index!\n";
 //![build_index1]
     clear(faiIndex);
 //![build_index2]
     CharString pathToFaiFile = pathToFile;
     append(pathToFaiFile, ".fai");
     if (!build(faiIndex, toCString(pathToFile), toCString(pathToFaiFile)))
-        std::cerr << "ERROR: Could not build the index!\n";
+        std::cout << "ERROR: Could not build the index!\n";
 //![build_index2]
 
 //![save_index]
     if (!save(faiIndex, toCString(pathToFaiFile)))
-       std::cerr << "ERROR: Could not save the index to file!\n";
+       std::cout << "ERROR: Could not save the index to file!\n";
 //![save_index]
 
 //![open_index1]
     if (!open(faiIndex, toCString(pathToFile)))
-       std::cerr << "ERROR: Could not load FAI index " << pathToFaiFile << "\n";
+       std::cout << "ERROR: Could not load FAI index " << pathToFaiFile << "\n";
 //![open_index1]
 
 //![open_index2]
     if (!open(faiIndex, toCString(pathToFile), toCString(pathToFaiFile)))
-       std::cerr << "ERROR: Could not load FAI index " << pathToFaiFile << "\n";
+       std::cout << "ERROR: Could not load FAI index " << pathToFaiFile << "\n";
 //![open_index2]
 
 //![idx]
     unsigned idx = 0;
     if (!getIdByName(idx, faiIndex, "chr1"))
-        std::cerr << "ERROR: FAI index has no entry for chr1.\n";
+        std::cout << "ERROR: FAI index has no entry for chr1.\n";
 //![idx]
 
 //![example_functions]
