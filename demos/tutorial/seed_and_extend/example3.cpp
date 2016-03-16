3ecec478a52e77a602ae4f06a06dfcec0ed1eacf
Jongkyu Kim <xenigmax@gmail.com>
2016-03-16 16:54:52 +0100

    an initial rework of seed extensions

--- demos/tutorial/seed_and_extend/example3.cpp
+++ demos/tutorial/seed_and_extend/example3.cpp
@@ -1,8 +1,9 @@
 //![header]
-#include <seqan/sequence.h>
+#include <seqan/align.h>
 #include <seqan/stream.h>
 #include <seqan/score.h>
 #include <seqan/seeds.h>
+#include <seqan/sequence.h>
 
 using namespace seqan;
 
@@ -10,28 +11,27 @@ int main()
 {
 //![header]
 //![example]
-    // The horizontal and vertical sequence (database and query).
+    // The horizontal and vertical sequence (subject and query sequences).
     CharString seqH = "The quick BROWN fox jumped again!";
     CharString seqV =     "thick BROWN boxes of brownies!";
-    //  ^^^
-    // Create seed and print the seeed sequence.
+    // Create the seed sequence.
     Seed<Simple> seed(11, 7, 14, 10);
-    std::cout << "original\n"
-              << "seedH: " << infix(seqH, beginPositionH(seed),
-                          endPositionH(seed)) << "\n"
-              << "seedV: " << infix(seqV, beginPositionV(seed),
-                          endPositionV(seed)) << "\n";
 
     // Perform match extension.
     Score<int, Simple> scoringScheme(1, -1, -1);
     extendSeed(seed, seqH, seqV, EXTEND_BOTH, scoringScheme, 3,
-               UnGappedXDrop());
-    // Print the resulting seed.
-    std::cout << "result\n"
-              << "seedH: " << infix(seqH, beginPositionH(seed),
-                          endPositionH(seed)) << "\n"
-              << "seedV: " << infix(seqV, beginPositionV(seed),
-                          endPositionV(seed)) << "\n";
+               GappedXDrop());
+
+    // Perform a banded alignment.
+    Align<CharString> align;
+    resize(rows(align), 2);
+    assignSource(row(align, 0), infix(seqH, beginPositionH(seed),
+                                      endPositionH(seed)));
+    assignSource(row(align, 1), infix(seqV, beginPositionV(seed),
+                                      endPositionV(seed)));
+
+    globalAlignment(align, scoringScheme);
+    std::cout << "Resulting alignment\n" << align << "\n";
 //![example]
 
 //![footer]
