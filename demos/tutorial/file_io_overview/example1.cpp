ce475f54618d069e59bd829513a27c824ab09c17
smehringer <svenja.mehringer@fu-berlin.de>
2016-03-18 13:39:41 +0100

    [DOC] FileIOOverview.rst - Some changes to inconsistent content. Some typos some style changes.

--- demos/tutorial/file_io_overview/example1.cpp
+++ demos/tutorial/file_io_overview/example1.cpp
@@ -7,15 +7,20 @@ int main()
 {
 //![include]
 //![ctor]
-    // Open input BAM file, BamFileIn supports both SAM and BAM files.
     CharString bamFileInName = getAbsolutePath("demos/tutorial/file_io_overview/example.bam");
     CharString samFileOutName = getAbsolutePath("demos/tutorial/file_io_overview/example.sam");
+
+    // Open input BAM file, BamFileIn supports both SAM and BAM files.
     BamFileIn bamFileIn(toCString(bamFileInName));
+
+    // Open output SAM file by passing the context of bamFileIn and the filename to open.
+    BamFileOut samFileOut(context(bamFileIn), toCString(samFileOutName));
 //![ctor]
 
 //![open]
-    // Open output SAM file by passing the context of bamFileIn and the filename to open.
-    BamFileOut samFileOut(context(bamFileIn), toCString(samFileOutName));
+    // Alternative way to open a bam or sam file
+    BamFileIn openBamFileIn;
+    open(openBamFileIn, toCString(bamFileInName));
 //![open]
 
 //![header]
