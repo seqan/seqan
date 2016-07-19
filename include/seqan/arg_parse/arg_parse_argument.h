2e0a6e1a0e14e96f7aa3a98dfb26370a692ae61f
smehringer <svenja.mehringer@gmail.com>
2016-07-19 13:45:03 +0200

    [COMPILER] include/arg_parse/ - added macro for deprecated objects to trigger compiler warning.

--- include/seqan/arg_parse/arg_parse_argument.h
+++ include/seqan/arg_parse/arg_parse_argument.h
@@ -136,6 +136,7 @@ public:
         OUTPUT_FILE,  // .. an outputfile (implicitly also a string)
         INPUT_PREFIX, // .. an inputprefix (implicitly also a string)
         ///@deprecated use INPUT_PREFIX instead
+        [[deprecated("Use INPUT_PREFIX instead.")]]
         INPUTPREFIX = INPUT_PREFIX,
         OUTPUT_PREFIX // .. an outoutprefix (implicitly also a string)
     };
