5e8c36ebce16a0048ff2f285f9c68f8e4fb6e2ba
rrahn <rene.rahn@fu-berlin.de>
2016-12-08 08:45:05 +0100

    [FEATURE] initial commit of generalized parallel dp.

--- include/seqan/align_parallel/align_parallel_impl.h
+++ include/seqan/align_parallel/align_parallel_impl.h
@@ -584,7 +584,7 @@ implParallelAlign(ExecutionPolicy<TParSpec, TVecSpec> const & execPolicy,
     typename impl::dp::parallel::ThreadLocalStorage<impl::dp::parallel::DPLocalStorage<TScoreValue, TSimdVec>, TParSpec>::Type tls;
 
 //    auto begin = sysTime();
-        
+    auto taskGraph = createGraph(taskContext, tls, execPolicy);
 //    std::cout << "\nCreation: " << std::setw(15) << sysTime() - begin << "s\n";
     invoke(taskGraph, execPolicy);
 
