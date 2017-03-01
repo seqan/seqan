9554e6ad5e407cb8c903d8c6b09707b00475ea52
rrahn <rene.rahn@fu-berlin.de>
2017-03-01 12:15:53 +0100

    [INTERNAL] Use unordered_map for thread local storage.

--- include/seqan/align_parallel/enumerable_thread_local.h
+++ include/seqan/align_parallel/enumerable_thread_local.h
@@ -62,7 +62,7 @@ public:
     //-------------------------------------------------------------------------
     // Member types.
 
-    using TMap           = std::map<std::thread::id, TValue>;
+    using TMap           = std::unordered_map<std::thread::id, TValue>;
     using TIterator      = Iter<EnumerableThreadLocal, EnumerableThreadLocalIterSpec>;
     using TConstIterator = Iter<EnumerableThreadLocal const, EnumerableThreadLocalIterSpec>;
 
