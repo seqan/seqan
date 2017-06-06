019772978b4c090409a64b78b94b33b3114950c7
rrahn <rene.rahn@fu-berlin.de>
2017-06-06 14:18:14 +0200

    [TEST] Adds test for extended globalAlignmentScore interface.

--- tests/align/test_align_simd.h
+++ tests/align/test_align_simd.h
@@ -40,206 +40,12 @@
 #include <seqan/basic.h>
 #include <seqan/align.h>
 
+#include "test_mock.h"
+
 namespace impl
 {
 namespace test_align_simd
 {
-
-struct TestAlignSimdVariableLength_;
-using VariableLengthSimd = seqan::Tag<TestAlignSimdVariableLength_>;
-
-struct TestAlignSimdEqualLength_;
-using EqualLengthSimd = seqan::Tag<TestAlignSimdEqualLength_>;
-
-template <typename TAlphabet, typename TSimdLength>
-struct TestSequences_;
-
-template <>
-struct TestSequences_<seqan::Dna, EqualLengthSimd>
-{
-    using TSeq = seqan::String<seqan::Dna>;
-
-    static auto
-    getSequences()
-    {
-        seqan::StringSet<TSeq> set;
-        appendValue(set, "AGCGACTGCAAACATCAGATCAGAG");
-        appendValue(set, "TAATACTAGCATGCGATAAGTCCCT");
-        appendValue(set, "GGCACGTGGATGGTTTAGAGGAATC");
-        appendValue(set, "AGATTCAAGTCTGGTTAACCATCAA");
-        appendValue(set, "ACAGGTCTTGAGTCTAAAATTGTCG");
-        appendValue(set, "TCTCCTGCGTACGAGATGGAAATAC");
-        appendValue(set, "TAGGTAACTACAGGGACTCCGACGT");
-        appendValue(set, "TATGTACGTTGCTCCGTCAGAGGCG");
-
-        appendValue(set, "CCATTCAGGATCACGTTACCGCGAA");
-        appendValue(set, "AAAAAGGGACCAGGAGCTCTTCTCC");
-        appendValue(set, "CCTGCGGTCACGTCTATAGAAATTA");
-        appendValue(set, "CACCATTAACCCTCCTGAGAACCGG");
-        appendValue(set, "GAGGCGGGAATCCGTCACGTATGAG");
-        appendValue(set, "AAGGTATTTGCCCGATAATCAATAC");
-        appendValue(set, "CCCAGGCTTCTAACTTTTTCCACTC");
-        appendValue(set, "GCTTGAGCCGGCTAGGCCTTTCTGC");
-
-        appendValue(set, "ATCTCGGGTCCTGCCCAACCGGTCT");
-        appendValue(set, "AACAAGGGACCAGGAGCTCTTCTCC");
-        appendValue(set, "ACACGCTAATATAGCGAATCACCGA");
-        appendValue(set, "GAACCCGGCGCCACGCAATGGAACG");
-        appendValue(set, "TCCTTAACTCCGGCAGGCAATTAAA");
-        appendValue(set, "ACAGAAAAATAGGCGAATGAATCTT");
-        appendValue(set, "GGGAACGTATGTATAACGCAAAAAA");
-        appendValue(set, "TTCTCTGTGTATCGAAGAATGGCCT");
-
-        appendValue(set, "CCGAAGTTTCGATGGACTGGTGCCA");
-        appendValue(set, "ACGCGCAGGCATAGTTTTAGGAGAA");
-        appendValue(set, "TTATTCGGGGGCAGTGACAACCAAC");
-
-        seqan::StringSet<TSeq>  set2(set);
-        std::sort(seqan::begin(set2, seqan::Standard()), seqan::end(set2, seqan::Standard()),
-                  [](auto& strA, auto& strB){ return seqan::isLess(strA, strB); });
-        return std::make_tuple(set, set2);
-    }
-};
-
-template <>
-struct TestSequences_<seqan::Dna, VariableLengthSimd>
-{
-    using TSeq = seqan::String<seqan::Dna>;
-
-    static auto
-    getSequences()
-    {
-        seqan::StringSet<TSeq> set;
-        appendValue(set, "AGCGACTGCAAACATCAGATCAGAGGTAGAG");
-        appendValue(set, "TAATACTAGCATGCGATAAGTCCCT");
-        appendValue(set, "GGCACGTGTGGTTTAGAGGAATC");
-        appendValue(set, "AGATTCAAGTCTGGTTAACCATCAA");
-        appendValue(set, "ACAGGTCTTGAGTCTAAAATTGTCGAA");
-        appendValue(set, "TCTCCTGCGTACGAGATGGAAATAC");
-        appendValue(set, "TAGGTAACTACAGGGACACGT");
-        appendValue(set, "TATGTACGTCTCCGTCAGAGGCG");
-
-        appendValue(set, "CCATTCAGGATCACGTTACCGCGAAGTACCC");
-        appendValue(set, "AAGGGACCAGGAGCTCTTCTCC");
-        appendValue(set, "CCTGCGGTCACGTCTATAGAAATT");
-        appendValue(set, "CACCATTAACCCTCCTGAGAACCGAGTAGG");
-        appendValue(set, "GAGGCGGGAATCCGTCACGTATGAG");
-        appendValue(set, "AAGGTATTTGCCCGATAATCAATACGATGAGATAGAGAGATAGAATAGAGAAGGGACCGCGCATGACTACGATCGACTGACTACGA");
-        appendValue(set, "CGAGTATATCGAGAGAGGTCACG");
-        appendValue(set, "GCTTGAGCCGGCTAGGCTCTGC");
-
-        appendValue(set, "ATCTCGGGTCCTGCCAACCGGTCT");
-        appendValue(set, "AAAAAGGGACCAGGAGCTCTTCTCC");
-        appendValue(set, "ACACGCTAATATAGCGAATCACCGA");
-        appendValue(set, "AATGGAACG");
-        appendValue(set, "TCCTTAACTCCGGCAGGCAATTATACCGGACTGACACTTAAA");
-        appendValue(set, "ACAGAAAAATAGGCGAATGAAACACTCTT");
-        appendValue(set, "GGGAACGTATGTATAACGCAAAAA");
-        appendValue(set, "TTCTCTGTGTATCGAAGAATGCT");
-
-        appendValue(set, "CCGAAGTTTCGATGGATGGATTCCACACACCTGGTGCCA");
-        appendValue(set, "ACGCGCAGGCATAGTTGGAGAA");
-        appendValue(set, "TTATTCGGGGGCAGTGACAACACTTAGCGACTAC");
-        
-        auto set2(set);
-        std::sort(seqan::begin(set2, seqan::Standard()), seqan::end(set2, seqan::Standard()),
-                  [](auto& strA, auto& strB){ return seqan::isLess(strA, strB); });
-        return std::make_tuple(set, set2);
-    }
-};
-
-template <>
-struct TestSequences_<seqan::AminoAcid, EqualLengthSimd>
-{
-    using TSeq = seqan::String<seqan::AminoAcid>;
-
-    static auto
-    getSequences()
-    {
-        seqan::StringSet<TSeq> set;
-        appendValue(set, "FNQSAEYPDISLHCGVLKWRATLGT");
-        appendValue(set, "EIKSDVLLHRPGNIGMQVAESYFAT");
-        appendValue(set, "PIIMWSMKNRTIERLPTGVLMISHT");
-        appendValue(set, "FMATNEKVHCACGADYQMIIDCNEA");
-        appendValue(set, "MFHQTSNANWMFVSNKFHIKFGTLD");
-        appendValue(set, "SNEMGQCFPHEPACFFDKDFRLFIN");
-        appendValue(set, "FPWAHYVVHTLREHRKDANHRSTSY");
-        appendValue(set, "QYRNTESMGCEMRCFTETIMIAGVA");
-
-        appendValue(set, "VVRMDGKEVLKQHVPTYADKHPTGQ");
-        appendValue(set, "TMLKWCEWCFAEFPPFASEPKFPPN");
-        appendValue(set, "GTWGWVDGVHHTMGEQCGPGRACWG");
-        appendValue(set, "ECDFQTWYFYCVNQEIFELFICCMG");
-        appendValue(set, "KRRELNGQERGGWWTVDGPGVSMGT");
-        appendValue(set, "CWAAHYVCWRTKQKQLVAFQRLNCI");
-        appendValue(set, "NRLVGFQIHCFLIRCVEPGQTHTID");
-        appendValue(set, "AYYVRGFMMGQMYGRPVILMTFTKP");
-
-        appendValue(set, "SFTQPVELHIPHYWWHLAYFMIMFY");
-        appendValue(set, "PMNKMFDFNNHQDLLTFTKRFPTPW");
-        appendValue(set, "VIPMIYHDWSIISALMMQKDIYYIA");
-        appendValue(set, "TPGMWGMATLTGNFNSIFVSKYVKN");
-        appendValue(set, "GKELWGMVIARAGMAVQNMYSRDTF");
-        appendValue(set, "VHASDLYAKCYSNCVYQENIDIAEV");
-        appendValue(set, "KQSGTLSGPQYWENVHRVLEDYPKE");
-        appendValue(set, "DPHGYCFYEGTFAWDVEVHEFNNKD");
-
-        appendValue(set, "NMQDVIGGKSLAQHSSVTYKAQQEH");
-        appendValue(set, "CQTPRWECSLNFDEKEAADLMIDVS");
-        appendValue(set, "PMMDLDHCMLIECLRPHNRDNCARH");
-
-        decltype(set) set2(set);
-        std::sort(seqan::begin(set2, seqan::Standard()), seqan::end(set2, seqan::Standard()),
-                  [](auto& strA, auto& strB){ return seqan::isLess(strA, strB); });
-        return std::make_tuple(set, set2);
-    }
-};
-
-template <>
-struct TestSequences_<seqan::AminoAcid, VariableLengthSimd>
-{
-    using TSeq = seqan::String<seqan::AminoAcid>;
-
-    static auto
-    getSequences()    {
-        seqan::StringSet<TSeq> set;
-        appendValue(set, "FNQSAEYPDISHCGVMQLKWRATLGT");
-        appendValue(set, "EIKSDVLLHRWSMKNPGNILMIDVGMQVAESYFAT");
-        appendValue(set, "PIIMWSMKNRTIEPTGLMISHT");
-        appendValue(set, "FMATNEKVHCACGWSMKNADLMIDVYQMIIDCNEA");
-        appendValue(set, "MWSMKNFHQTSNANWMFVSNMQKFHIKFGTLD");
-        appendValue(set, "SNEGQCFPHEPACFWSMKFDKDFRLFIN");
-        appendValue(set, "FPWAHYVVHTLREHMQRKDANHRSTSY");
-        appendValue(set, "QYRNTWSMKNESMGCEMRFLMIVTETIMIAGVA");
-
-        appendValue(set, "VVRMDGKEVLWSMKNKQHVPTYADKHPTGQ");
-        appendValue(set, "TMLKWCEWCFALMIDVEFPPFASEPKFPPN");
-        appendValue(set, "GTWGVDGVHHWSMWSMKNLMIDVTMGEQCGPGRACWG");
-        appendValue(set, "ECDFQTWYFYCVNQMQEIFELFICCMG");
-        appendValue(set, "KRREWSMKNLNGQERGGWWTVDGPGVSMGT");
-        appendValue(set, "CWAAHYCWRWWSMKNSMKNTKLMIDVQMQKQLVAFQRLNCI");
-        appendValue(set, "NRLVGFQIHCFIRCVEPGQTHTID");
-        appendValue(set, "AYYVRGFMMGQMMQYGRPVILMTFTKP");
-
-        appendValue(set, "SFTQPVELHIPHYWLMIDVWHLAYFMIMFY");
-        appendValue(set, "PMNKMFDFNHQMQDLLTFTKPTPW");
-        appendValue(set, "VIPMIYHDWSIISALMMLMIDVQKDIYYIA");
-        appendValue(set, "TPGMWGMATLTGMQNFNSFVSKYVKN");
-        appendValue(set, "GKELWGMVIARAGMAVQNLMIDVMYSRDTF");
-        appendValue(set, "VHASDLWSNYAKCYSNCVYQEIDIAEV");
-        appendValue(set, "KQSGTLSMQGPYWENVHRVLLMIDVEDYPKE");
-        appendValue(set, "DPHGYCFMQYEGTFAWDVEVHEFNNKD");
-
-        appendValue(set, "NMQDVIGGKSLAQHSSVTYAQQEH");
-        appendValue(set, "CQTPRWECMQSLNFDEKEAADLMIDVS");
-        appendValue(set, "PMMDLDWSMKNMLIECLRPHNRMQDNLMIDVCARH");
-        
-        auto set2(set);
-        std::sort(seqan::begin(set2, seqan::Standard()), seqan::end(set2, seqan::Standard()),
-                  [](auto& strA, auto& strB){ return seqan::isLess(strA, strB); });
-        return std::make_tuple(set, set2);
-    }
-};
 
 struct LocalAlignTester_
 {
@@ -325,18 +131,18 @@ class SimdAlignTestCommon : public SimdAlignTest<T>
 {};
 
 typedef
-        seqan::TagList<std::tuple<seqan::AlignConfig<>,                         impl::test_align_simd::EqualLengthSimd,     seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<>,                         impl::test_align_simd::VariableLengthSimd,  seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<true, true, true, true>,   impl::test_align_simd::EqualLengthSimd,     seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<true, true, true, true>,   impl::test_align_simd::VariableLengthSimd,  seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<true, false, false, true>, impl::test_align_simd::EqualLengthSimd,     seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<true, false, false, true>, impl::test_align_simd::VariableLengthSimd,  seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<false, true, true, false>, impl::test_align_simd::EqualLengthSimd,     seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<false, true, true, false>, impl::test_align_simd::VariableLengthSimd,  seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<>,                         impl::test_align_simd::EqualLengthSimd,     seqan::BandOn>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<true, true, true, true>,   impl::test_align_simd::EqualLengthSimd,     seqan::BandOn>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<true, false, false, true>, impl::test_align_simd::EqualLengthSimd,     seqan::BandOn>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<false, true, true, false>, impl::test_align_simd::EqualLengthSimd,     seqan::BandOn>
+        seqan::TagList<std::tuple<seqan::AlignConfig<>,                         impl::test_align_mock::EqualLengthSimd,     seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<>,                         impl::test_align_mock::VariableLengthSimd,  seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<true, true, true, true>,   impl::test_align_mock::EqualLengthSimd,     seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<true, true, true, true>,   impl::test_align_mock::VariableLengthSimd,  seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<true, false, false, true>, impl::test_align_mock::EqualLengthSimd,     seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<true, false, false, true>, impl::test_align_mock::VariableLengthSimd,  seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<false, true, true, false>, impl::test_align_mock::EqualLengthSimd,     seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<false, true, true, false>, impl::test_align_mock::VariableLengthSimd,  seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<>,                         impl::test_align_mock::EqualLengthSimd,     seqan::BandOn>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<true, true, true, true>,   impl::test_align_mock::EqualLengthSimd,     seqan::BandOn>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<true, false, false, true>, impl::test_align_mock::EqualLengthSimd,     seqan::BandOn>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<false, true, true, false>, impl::test_align_mock::EqualLengthSimd,     seqan::BandOn>
         > > > > > > > > > > > > SimdAlignTestCommonCommonTypes;
 
 SEQAN_TYPED_TEST_CASE(SimdAlignTestCommon, SimdAlignTestCommonCommonTypes);
@@ -350,9 +156,9 @@ class SimdAlignLocalTestCommon : public SimdAlignTest<T>
 {};
 
 typedef
-        seqan::TagList<std::tuple<seqan::AlignConfig<>, impl::test_align_simd::EqualLengthSimd,    seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<>, impl::test_align_simd::VariableLengthSimd, seqan::BandOff>,
-        seqan::TagList<std::tuple<seqan::AlignConfig<>, impl::test_align_simd::EqualLengthSimd,    seqan::BandOn>
+        seqan::TagList<std::tuple<seqan::AlignConfig<>, impl::test_align_mock::EqualLengthSimd,    seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<>, impl::test_align_mock::VariableLengthSimd, seqan::BandOff>,
+        seqan::TagList<std::tuple<seqan::AlignConfig<>, impl::test_align_mock::EqualLengthSimd,    seqan::BandOn>
         > > > SimdAlignLocalTestCommonCommonTypes;
 
 SEQAN_TYPED_TEST_CASE(SimdAlignLocalTestCommon, SimdAlignLocalTestCommonCommonTypes);
@@ -373,7 +179,7 @@ void testAlignSimd(TFunctor const &,
                    int const lDiag = std::numeric_limits<int>::min(),
                    int const uDiag = std::numeric_limits<int>::max())
 {
-    auto sets = impl::test_align_simd::TestSequences_<TAlphabet, TSimdLength>::getSequences();
+    auto sets = impl::test_align_mock::TestSequences_<TAlphabet, TSimdLength>::getSequences();
 
     // Prepare an align object with the sequences.
     seqan::StringSet<seqan::Align<seqan::String<TAlphabet> > > alignments;
@@ -443,7 +249,7 @@ void testAlignSimdScore(TTester const &,
                         int const lDiag = std::numeric_limits<int>::min(),
                         int const uDiag = std::numeric_limits<int>::max())
 {
-    auto sets = impl::test_align_simd::TestSequences_<TAlphabet, TSimdLength>::getSequences();
+    auto sets = impl::test_align_mock::TestSequences_<TAlphabet, TSimdLength>::getSequences();
 
     seqan::String<TScoreValue> scores = TTester::run(std::get<0>(sets), std::get<1>(sets), score, config, lDiag, uDiag);
 
